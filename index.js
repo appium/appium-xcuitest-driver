96c05811f7ee0a44139b28e8cc39b76bac3cce9f
Jonathan Lipps <jlipps@gmail.com>
2015-09-17 15:10:03 -0700

    initial architecture and server

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-import {default as sample} from './lib/sample';
+// transpile:main
 
-export {sample};
+import yargs from 'yargs';
+import { asyncify } from 'asyncbox';
+import { WebDriverAgentDriver } from './lib/driver';
+import { startServer } from './lib/server';
+
+const DEFAULT_HOST = "localhost";
+const DEFAULT_PORT = 4994;
+
+async function main () {
+  let port = yargs.argv.port || DEFAULT_PORT;
+  let host = yargs.argv.host || DEFAULT_HOST;
+  return startServer(port, host);
+}
+
+if (require.main === module) {
+  asyncify(main);
+}
+
+export { WebDriverAgentDriver, startServer };
 
