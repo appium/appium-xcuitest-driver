8bd7dcbbfc6d0594eaaff7e508fdb6a3ef3b99ae
Jonah Stiennon <jonahss@gmail.com>
2015-10-19 15:36:36 -0700

    custom agent binary path

--- test/unit/webdriveragent-specs.js
+++ test/unit/webdriveragent-specs.js
@@ -1,14 +1,27 @@
 import WebDriverAgent from '../../lib/webdriveragent';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import { fs } from 'appium-support';
 
 chai.should();
 chai.use(chaiAsPromised);
 
-describe('WebDriverAgent', () => {
+let fakeConstructorArgs = {
+  udid: '4',
+  platformVersion: '9',
+  host: 'me',
+  port: '5000',
+};
+
+describe('Constructor', () => {
   it('should require all parameters', () => {
     (() => {
       new WebDriverAgent();
     }).should.throw(/must send in the 'udid'/);
+  });
+
+  it('should have a binary for the webdriver agent', async () => {
+    let agent = new WebDriverAgent(fakeConstructorArgs);
+    await fs.exists(agent.agentPath);
   });
 });
