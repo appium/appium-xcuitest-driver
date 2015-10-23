39f3539c0e0d4f3f5e3bb202db82a3d9daed8557
Jonah Stiennon <jonahss@gmail.com>
2015-10-23 13:24:18 -0700

    webdriver.js should error if a sim is not booted

--- test/functional/webdriveragent-e2e-specs.js
+++ test/functional/webdriveragent-e2e-specs.js
@@ -12,36 +12,54 @@ const PLATFORM_VERSION = '9.0';
 let testUrl = 'http://localhost:8100/tree';
 
 describe('WebDriverAgentDriver', () => {
-  let sim;
-  before(async function () {
-    this.timeout(30 * 1000);
-    let simUdid = await createDevice('webDriverAgentTest', 'iPhone 6', PLATFORM_VERSION);
-    sim = await getSimulator(simUdid);
-  });
-
-  after(async function () {
-    this.timeout(20 * 1000);
-    await deleteDevice(sim.udid);
-  });
-
-  afterEach(async function () {
-    this.timeout(30 * 1000);
-    await sim.shutdown();
-  });
-
-  it('should launch agent on a sim', async function () {
-    this.timeout(90 * 1000);
-    await sim.run();
-    let agent = new WebDriverAgent({
-      udid: sim.udid,
-      platformVersion: PLATFORM_VERSION,
-      host: 'localhost',
-      port: 8100,
-      agentPath: process.env.AGENT_PATH
+  describe('with fresh sim', () => {
+    let sim;
+    before(async function () {
+      this.timeout(30 * 1000);
+      let simUdid = await createDevice('webDriverAgentTest', 'iPhone 6', PLATFORM_VERSION);
+      sim = await getSimulator(simUdid);
     });
 
-    await agent.launch('sessionId');
-    console.log('requesting url now');
-    await request(testUrl);
+    after(async function () {
+      this.timeout(20 * 1000);
+      await deleteDevice(sim.udid);
+    });
+
+    describe('with running sim', () => {
+      afterEach(async function () {
+        this.timeout(30 * 1000);
+        await sim.shutdown();
+      });
+
+      it('should launch agent on a sim', async function () {
+        this.timeout(90 * 1000);
+        await sim.run();
+        let agent = new WebDriverAgent({
+          udid: sim.udid,
+          platformVersion: PLATFORM_VERSION,
+          host: 'localhost',
+          port: 8100,
+          agentPath: process.env.AGENT_PATH
+        });
+
+        await agent.launch('sessionId');
+        await request(testUrl);
+      });
+    });
+
+    describe('with sim not booted', () => {
+      it('should exit if sim is not booted', async function () {
+        this.timeout(20 * 1000);
+        let agent = new WebDriverAgent({
+          udid: sim.udid,
+          platformVersion: PLATFORM_VERSION,
+          host: 'localhost',
+          port: 8100,
+          agentPath: process.env.AGENT_PATH
+        });
+
+        await agent.launch('sessionId').should.be.rejectedWith('is not in \'booted\'');
+      });
+    });
   });
 });
