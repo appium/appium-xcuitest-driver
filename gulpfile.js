ee77ce675b4d1d9789e5a6f1dc33e0b981082d50
Jonah Stiennon <jonahss@gmail.com>
2015-12-09 17:25:33 -0800

    potentially fix npm-install of this module

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,11 @@
 "use strict";
 
-try {
-  var copySymlinks = require('./build/lib/copySymlinks.js').copySymlinks;
-} catch (e) {
-  // we catch this, so that `gulp watch` can keep looping.
-}
-
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 gulp.task('prepareSymlinkedHeaders', function () {
+  // require copySymlinks here so that it's transpiled first.
+  var copySymlinks = require('./build/lib/copySymlinks.js').copySymlinks;
   return gulp.src('./WebDriverAgent/Pods/Headers/**/*.h', { read: false })
     .pipe(copySymlinks());
 });
