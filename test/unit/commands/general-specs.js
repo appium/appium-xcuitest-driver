624e88868b8b53ce47f74c3dc9ac226b99befaf2
Isaac Murchie <isaac@saucelabs.com>
2016-07-12 13:26:40 -0700

    Add method to handle touch id

--- test/unit/commands/general-specs.js
+++ test/unit/commands/general-specs.js
@@ -18,4 +18,28 @@ describe('general commands', () => {
       proxySpy.firstCall.args[1].should.eql('POST');
     });
   });
+
+  describe('touch id', () => {
+    it('should send translated POST request to WDA', () => {
+      driver.touchId();
+      proxySpy.calledOnce.should.be.true;
+      proxySpy.firstCall.args[0].should.eql('/simulator/touch_id');
+      proxySpy.firstCall.args[1].should.eql('POST');
+      proxySpy.firstCall.args[2].should.eql({match: true});
+    });
+    it('should send translated POST request to WDA with true', () => {
+      driver.touchId(true);
+      proxySpy.calledOnce.should.be.true;
+      proxySpy.firstCall.args[0].should.eql('/simulator/touch_id');
+      proxySpy.firstCall.args[1].should.eql('POST');
+      proxySpy.firstCall.args[2].should.eql({match: true});
+    });
+    it('should send translated POST request to WDA with false', () => {
+      driver.touchId(false);
+      proxySpy.calledOnce.should.be.true;
+      proxySpy.firstCall.args[0].should.eql('/simulator/touch_id');
+      proxySpy.firstCall.args[1].should.eql('POST');
+      proxySpy.firstCall.args[2].should.eql({match: false});
+    });
+  });
 });
