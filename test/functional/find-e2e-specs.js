654eb85937344fc3c3220b35e9a964568691f79c
Isaac Murchie <isaac@saucelabs.com>
2016-07-12 10:13:31 -0700

    Add initial tests for find commands

---
+++ test/functional/find-e2e-specs.js
@@ -0,0 +1,323 @@
+import { startServer } from '../..';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import wd from 'wd';
+import B from 'bluebird';
+import _ from 'lodash';
+import { UICATALOG_CAPS, TESTAPP_CAPS } from './desired';
+
+
+chai.should();
+chai.use(chaiAsPromised);
+
+const HOST = "localhost",
+      PORT = 4994;
+
+async function clickButton (driver, name) {
+  let el = (await driver.elementsByXPath(`//XCUIElementTypeButton[@name = '${name}']`))[0];
+  if (el && (await el.isDisplayed())) {
+    await el.click();
+  }
+}
+
+describe('XCUITestDriver - find', function () {
+  this.timeout(200 * 1000);
+
+  let server, driver;
+  before(async () => {
+    driver = wd.promiseChainRemote(HOST, PORT);
+    server = await startServer(PORT, HOST);
+    await driver.init(TESTAPP_CAPS);
+  });
+  after(async () => {
+    await driver.quit();
+    await server.close();
+  });
+
+  describe('by id', () => {
+    it('should find a single element by id', async () => {
+      let el = await driver.elementById('ComputeSumButton');
+      el.should.exist;
+    });
+
+    it('should find a single element by id wrapped in array for multi', async () => {
+      let els = await driver.elementsById('ComputeSumButton');
+      els.should.have.length(1);
+    });
+
+    it('should first attempt to match accessibility id', async () => {
+      let el = await driver.elementById('ComputeSumButton');
+      (await el.getAttribute('label')).should.equal('Compute Sum');
+    });
+
+    it('should attempt to match by string if no accessibility id matches', async () => {
+      let el = await driver.elementById('Compute Sum');
+      (await el.getAttribute('label')).should.equal('Compute Sum');
+    });
+
+    it.skip('should use a localized string if the id is a localization key', async () => {
+      let el = await driver.elementById('main.button.computeSum');
+      (await el.getAttribute('label')).should.equal('Compute Sum');
+    });
+
+    it.skip('should be able to return multiple matches', async () => {
+      let els = await driver.elementsById('TextField');
+      els.length.should.be.greaterThan(1);
+    });
+  });
+});
+
+describe('XCUITestDriver - find', function () {
+  this.timeout(200 * 1000);
+
+  let server, driver;
+  before(async () => {
+    driver = wd.promiseChainRemote(HOST, PORT);
+    server = await startServer(PORT, HOST);
+    await driver.init(UICATALOG_CAPS);
+  });
+  after(async () => {
+    await driver.quit();
+    await server.close();
+  });
+
+  // establish that the basic things work as we imagine
+  describe('basics', () => {
+    let el1;
+    before(async () => {
+      el1 = await driver.elementByAccessibilityId('Buttons');
+      el1.should.exist;
+    });
+    it('should find an element within descendants', async () => {
+      let el2 = await el1.elementByClassName('XCUIElementTypeStaticText');
+      (await el2.getAttribute('name')).should.contain("Buttons");
+    });
+
+    it('should not find an element not within itself', async () => {
+      await B.resolve(el1.elementByClassName('XCUIElementTypeNavigationBar'))
+        .should.be.rejectedWith(/Error response status: 7/);
+    });
+
+    it.skip('should find some elements within itself', async () => {
+      let els = await el1.elementsByClassName('XCUIElementTypeStaticText');
+      els.should.have.length(2);
+    });
+
+    it('should not find elements not within itself', async () => {
+      let els = await el1.elementsByClassName('XCUIElementTypeNavigationBar');
+      els.should.have.length(0);
+    });
+  });
+
+  // make sure that elements are mixed up
+  describe.skip('no mix up', () => {
+    after(async() => {
+      await clickButton(driver, 'UICatalog');
+    });
+
+    it('should not allow found elements to be mixed up', async () => {
+      let table = await driver.elementByClassName('XCUIElementTypeTable');
+      let el1 = await table.elementByClassName('XCUIElementTypeStaticText');
+      let el1Name = await el1.getAttribute('name');
+      await el1.click();
+
+      // we need a hard pause, because if we haven't shifted views yet
+      // we will have the previous elements, so the get command will be fulfilled.
+      await B.delay(1000);
+
+      await driver.setImplicitWaitTimeout(5000);
+      table = await driver.elementByClassName('XCUIElementTypeTable');
+      let el2 = await driver.elementByClassName('XCUIElementTypeStaticText');
+      let el2Name = await el2.getAttribute('name');
+      el1.should.not.equal(el2);
+      el1Name.should.not.equal(el2Name);
+
+      // el1 is gone, so it doesn't have a name anymore
+      (await el1.getAttribute('name')).should.equal("");
+    });
+  });
+
+  describe('by xpath', () => {
+    describe('individual calls', function () {
+      before(async () => {
+        // before anything, try to go back
+        // otherwise the tests will fail erroneously
+        await clickButton(driver, 'UICatalog');
+
+        // and make sure we are at the top of the page
+        try {
+          await driver.execute('mobile: scroll', {direction: 'up'});
+        } catch (ign) {}
+      });
+      beforeEach(async () => {
+        // go into the right page
+        let el = await driver.elementByAccessibilityId('Buttons');
+        await el.click();
+      });
+      afterEach(async () => {
+        await clickButton(driver, 'UICatalog');
+      });
+
+      it('should respect implicit wait', async () => {
+        await driver.setImplicitWaitTimeout(5000);
+
+        let begin = Date.now();
+        await driver.elementByXPath('//something_not_there')
+          .should.eventually.be.rejected;
+        (Date.now() - begin).should.be.above(5000);
+      });
+      it.skip('should return the last button', async () => {
+        let el = await driver.elementByXPath('//XCUIElementTypeButton[last()]');
+        (await el.getAttribute('name')).should.equal('Button'); // this is the name of the last button
+      });
+      it('should return a single element', async () => {
+        let el = await driver.elementByXPath('//XCUIElementTypeButton');
+        (await el.getAttribute('name')).should.equal('UICatalog');
+      });
+      it('should return multiple elements', async () => {
+        let els = await driver.elementsByXPath('//XCUIElementTypeButton');
+        els.should.have.length.above(5);
+      });
+      it('should filter by name', async () => {
+        let el = await driver.elementByXPath("//XCUIElementTypeButton[@name='X Button']");
+        (await el.getAttribute('name')).should.equal('X Button');
+      });
+      it('should know how to restrict root-level elements', async () => {
+        await driver.elementByXPath('/XCUIElementTypeButton').should.be.rejectedWith(/NoSuchElement/);
+      });
+      it('should search an extended path by child', async () => {
+        let el = await driver.elementByXPath('//XCUIElementTypeNavigationBar/XCUIElementTypeStaticText');
+        (await el.getAttribute('name')).should.equal('Buttons');
+      });
+      it('should search an extended path by descendant', async () => {
+        let els = await driver.elementsByXPath('//XCUIElementTypeTable//XCUIElementTypeButton');
+        let texts = await B.all(_.map(els, (el) => el.getAttribute('name')));
+        texts.should.not.include('UICatalog');
+        texts.should.include('X Button');
+      });
+      it.skip('should filter by indices', async () => {
+        await driver.setImplicitWaitTimeout(10000);
+        let el = await driver.elementByXPath('//XCUIElementTypeTable[1]//XCUIElementTypeButton[4]');
+        (await el.getAttribute('name')).should.equal('X Button');
+      });
+
+      it('should filter by partial text', async () => {
+        let el = await driver.elementByXPath("//XCUIElementTypeTable//XCUIElementTypeButton[contains(@name, 'X ')]");
+        (await el.getAttribute('name')).should.equal('X Button');
+      });
+    });
+
+    describe.skip('multiple calls', function () {
+      let runs = 5;
+
+      before(async () => {
+        // go into the right page
+        let el = await driver.elementByAccessibilityId('Buttons');
+        await el.click();
+      });
+      after(async () => {
+        await clickButton(driver, 'UICatalog');
+      });
+
+      let test = function (path, minLength) {
+        return function () {
+          it('should not crash', async () => {
+            let els = await driver.elementsByXPath(path);
+            els.should.have.length.above(minLength);
+          });
+        };
+      };
+
+      describe.skip('finding specific path', function () {
+        for (let n = 0; n < runs; n++) {
+          describe(`test ${n + 1}`, test('//XCUIElementTypeApplication[0]/XCUIElementTypeWindow[0]', 17));
+        }
+      });
+
+      describe('finding //*', function () {
+        for (let n = 0; n < runs; n++) {
+          describe(`test ${n + 1}`, test("//*", 52));
+        }
+      });
+    });
+  });
+
+  describe('by accessibility id', () => {
+    afterEach(async () => {
+      await clickButton(driver, 'UICatalog');
+    });
+
+    it('should find one element', async () => {
+      let el1 = await driver.elementByAccessibilityId('Action Sheets');
+      await el1.click();
+      let el2 = await driver.elementByAccessibilityId('Okay / Cancel');
+      (await el2.getAttribute('name')).should.equal('Okay / Cancel');
+    });
+
+    it.skip('should find several elements', async () => {
+      let el1 = await driver.elementByAccessibilityId('Action Sheets');
+      await el1.click();
+      let els = await driver.elementsByAccessibilityId('Okay / Cancel');
+      els.should.have.length(2);
+    });
+
+    it('should find an element beneath another element', async () => {
+      let el1 = await driver.elementByClassName('XCUIElementTypeTable');
+      let el2 = await el1.elementByAccessibilityId('Action Sheets');
+      el2.should.exist;
+    });
+  });
+
+  describe('by class name', () => {
+    it('should return all image elements with internally generated ids', async () => {
+      let els = await driver.elementsByClassName('XCUIElementTypeImage');
+      els.length.should.be.above(0);
+      for (let el of els) {
+        el.should.exist;
+      }
+    });
+
+    describe('findElementsByClassName textfield case', () => {
+      after(async () => {
+        await clickButton(driver, 'UICatalog');
+      });
+
+      it('should find only one textfield', async () => {
+        let el1 = await driver.elementByAccessibilityId('Action Sheets');
+        await el1.click();
+        let el2 = await driver.elementByAccessibilityId('Okay / Cancel');
+        let els = await el2.elementsByClassName('XCUIElementTypeStaticText');
+        els.should.have.length(1);
+      });
+    });
+  });
+
+  describe('duplicate text field', () => {
+    beforeEach(async () => {
+      try {
+        let element = await driver.elementByClassName('XCUIElementTypeTable');
+        await driver.execute('mobile: scroll', {element, direction: 'down', name: 'Text Fields'});
+      } catch (ign) {}
+      await driver.setImplicitWaitTimeout(5000);
+    });
+    afterEach(async () => {
+      await clickButton(driver, 'UICatalog');
+    });
+
+    it('should find only one element per text field', async () => {
+      let el = await driver.elementByAccessibilityId('Text Fields');
+      await el.click();
+
+      let els = await driver.elementsByClassName('XCUIElementTypeTextField');
+      els.should.have.length(4);
+    });
+
+    it('should find only one element per secure text field', async () => {
+      let el = await driver.elementByAccessibilityId('Text Fields');
+      await el.click();
+
+      let els = await driver.elementsByClassName('XCUIElementTypeSecureTextField');
+      els.should.have.length(1);
+    });
+  });
+});
