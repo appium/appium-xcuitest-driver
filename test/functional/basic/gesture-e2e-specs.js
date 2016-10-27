372d5bc14cd2c3123156d03ea8c7b842224597d6
Isaac Murchie <isaac@saucelabs.com>
2016-10-27 15:27:03 +0200

    Fix gesture support a bit

--- test/functional/basic/gesture-e2e-specs.js
+++ test/functional/basic/gesture-e2e-specs.js
@@ -2,9 +2,10 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import B from 'bluebird';
 import wd from 'wd';
+import _ from 'lodash';
 import { UICATALOG_CAPS } from '../desired';
-import { clickButton } from '../helpers/navigation';
 import { initSession, deleteSession } from '../helpers/session';
+import { clickButton } from '../helpers/navigation';
 
 
 chai.should();
@@ -14,17 +15,60 @@ describe('XCUITestDriver - gestures', function () {
   this.timeout(200 * 1000);
 
   let driver;
-  before(async () => {
-    driver = await initSession(UICATALOG_CAPS);
-  });
-  after(async () => {
-    await deleteSession();
-  });
 
-  // make sure that elements are mixed up
-  describe('tap', () => {
-    afterEach(async() => {
+  describe('tap and press', () => {
+    before(async () => {
+      driver = await initSession(UICATALOG_CAPS);
+    });
+    beforeEach(async () => {
+      let el = await driver.elementByAccessibilityId('Action Sheets');
+      await el.click();
+    });
+    after(async () => {
+      await deleteSession();
+    });
+    afterEach(async () => {
+      // wait a moment to allow alert to be closed
+      await B.delay(1000);
       await clickButton(driver, 'UICatalog');
+    });
+
+    it('should tap on the element', async () => {
+      let el1 = await driver.elementByAccessibilityId('Okay / Cancel');
+      let action = new wd.TouchAction(driver);
+      action.tap({el: el1});
+      await action.perform();
+
+      let el3 = await driver.elementByAccessibilityId('OK');
+      await el3.click();
+    });
+    it('should long press on an element', async () => {
+      let el1 = await driver.elementByAccessibilityId('Okay / Cancel');
+      let action = new wd.TouchAction(driver);
+      action.longPress({el: el1});
+      await action.perform();
+
+      let el3 = await driver.elementByAccessibilityId('Cancel');
+      await el3.click();
+    });
+    it('should long press on an element with duration through press-wait-release', async () => {
+      let el1 = await driver.elementByAccessibilityId('Okay / Cancel');
+      let action = new wd.TouchAction(driver);
+      action.press({el: el1}).wait(1200).release();
+      await action.perform();
+
+      let el3 = await driver.elementByAccessibilityId('Cancel');
+      await el3.click();
+    });
+  });
+  describe('tap with tapWithShortPressDuration cap', () => {
+    before(async () => {
+      driver = await initSession(_.defaults({
+        tapWithShortPressDuration: 0.01
+      }, UICATALOG_CAPS));
+    });
+    after(async () => {
+      await deleteSession();
     });
 
     it('should tap on the element', async () => {
@@ -33,7 +77,7 @@ describe('XCUITestDriver - gestures', function () {
       action.tap({el: el1});
       action.perform();
 
-      // pause a moment
+      // pause a moment so the alert can animate
       await B.delay(500);
 
       let el2 = await driver.elementByAccessibilityId('Okay / Cancel');
