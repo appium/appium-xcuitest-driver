7f402d91e73ac0c291e08b2509ec7c56d2fbbfff
Dan Graham <dan@saucelabs.com>
2017-03-08 15:20:19 -0800

    Starts IWDP if real device and cap is set
    * If startIWDP === true, start ios_webkit_debug_proxy
    * Add test to confirm that Safari page can be opened and interacted with when startIWDP == true
    * Add test to confirm that interactions with Safari page are rejected when startIWDP == false
    * Fixed object-shorthand lint error in general.js

---
+++ test/functional/basic/iwdp-e2e-specs.js
@@ -0,0 +1,39 @@
+// transpile:mocha
+
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { SAFARI_CAPS } from '../desired';
+import { initSession, deleteSession, MOCHA_TIMEOUT } from '../helpers/session';
+import B from 'bluebird';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+if (process.env.REAL_DEVICE) {
+  describe('ios_webkit_debug_proxy: ', function () {
+    this.timeout(MOCHA_TIMEOUT);
+    let caps, driver;
+
+    beforeEach(async () => {
+      caps = Object.assign(SAFARI_CAPS);
+      caps.startIWDP = true;
+    });
+
+    afterEach(async () => {
+      await deleteSession();
+      await B.delay(2000);
+    });
+
+    it('Should start a Safari session if "caps.startIWDP === true"', async () => {
+      caps.startIWDP = true;
+      driver = await initSession(caps);
+      await driver.source().should.not.be.rejected;
+      await driver.quit();
+    });
+
+    it('Should not start a Safari session if "caps.startIWDP === false"', async () => {
+      caps.startIWDP = false;
+      await initSession(caps).should.be.rejected;
+    });
+  });
+}
