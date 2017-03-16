9d4732fca975b0a2b8a4a2a77df183c90d70a770
Mykola Mokhnach <nick+1@wire.com>
2017-03-16 11:51:51 +0100

    Make all XCTest gestures available from mobile: interface

--- test/unit/commands/gesture-specs.js
+++ test/unit/commands/gesture-specs.js
@@ -88,20 +88,199 @@ describe('gesture commands', () => {
     });
 
     describe('swipe', () => {
+      const commandName = 'swipe';
+
       it('should throw an error if no direction is specified', async () => {
-        await driver.execute('mobile: swipe', {element: 4}).should.be.rejectedWith(/Error: Mobile swipe requires direction/);
+        await driver.execute(`mobile: ${commandName}`, {element: 4})
+          .should.be.rejectedWith(/Error: Mobile swipe requires direction/);
       });
 
       it('should throw an error if invalid direction', async () => {
-        await driver.execute('mobile: swipe', {element: 4, direction: 'foo'}).should.be.rejectedWith(/Error: Direction must be up, down, left or right/);
+        await driver.execute(`mobile: ${commandName}`, {element: 4, direction: 'foo'})
+          .should.be.rejectedWith(/Error: Direction must be up, down, left or right/);
       });
 
       it('should proxy a swipe up request through to WDA', async () => {
-        await driver.execute('mobile: swipe', {element: 4, direction: 'up'});
+        await driver.execute(`mobile: ${commandName}`, {element: 4, direction: 'up'});
         proxySpy.calledOnce.should.be.true;
         proxySpy.firstCall.args[0].should.eql('/wda/element/4/swipe');
         proxySpy.firstCall.args[1].should.eql('POST');
         return proxySpy.firstCall.args[2].should.eql({direction: 'up'});
+      });
+    });
+
+    describe('pinch', () => {
+      const commandName = 'pinch';
+
+      it('should throw an error if no mandatory parameter is specified', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4, scale: 4.1})
+          .should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {element: 4, velocity: -0.5})
+          .should.be.rejectedWith(/parameter is mandatory/);
+      });
+
+      it('should throw an error if param is invalid', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4, scale: '', velocity: 1})
+          .should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {element: 4, scale: 0, velocity: null})
+          .should.be.rejectedWith(/should be a valid number/);
+      });
+
+      it('should proxy a pinch request through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4, scale: 1, velocity: '1'});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/element/4/pinch');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['scale', 'velocity']);
+      });
+    });
+
+    describe('doubleTap', () => {
+      const commandName = 'doubleTap';
+
+      it('should throw an error if no mandatory parameter is specified', async () => {
+        await driver.execute(`mobile: ${commandName}`, {x: 100}).should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {y: 200}).should.be.rejectedWith(/parameter is mandatory/);
+      });
+
+      it('should throw an error if param is invalid', async () => {
+        await driver.execute(`mobile: ${commandName}`, {x: '', y: 1}).should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {x: 1, y: null}).should.be.rejectedWith(/should be a valid number/);
+      });
+
+      it('should proxy a doubleTap request for an element through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/element/4/doubleTap');
+        proxySpy.firstCall.args[1].should.eql('POST');
+      });
+
+      it('should proxy a doubleTap request for a coordinate point through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {x: 100, y: 100});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/doubleTap');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['x', 'y']);
+      });
+    });
+
+    describe('twoFingerTap', () => {
+      const commandName = 'twoFingerTap';
+
+      it('should proxy a twoFingerTap request for an element through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/element/4/twoFingerTap');
+        proxySpy.firstCall.args[1].should.eql('POST');
+      });
+    });
+
+    describe('touchAndHold', () => {
+      const commandName = 'touchAndHold';
+
+      it('should throw an error if no mandatory parameter is specified', async () => {
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, x: 1}).should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, y: 200}).should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {x: 100, y: 200}).should.be.rejectedWith(/parameter is mandatory/);
+      });
+
+      it('should throw an error if param is invalid', async () => {
+        await driver.execute(`mobile: ${commandName}`, {duration: '', x: 1, y: 1}).should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 1, x: '', y: 1}).should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 1, x: 1, y: null}).should.be.rejectedWith(/should be a valid number/);
+      });
+
+      it('should proxy a touchAndHold request for an element through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4, duration: 100});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/element/4/touchAndHold');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['duration']);
+      });
+
+      it('should proxy a touchAndHold request for a coordinate point through to WDA', async () => {
+        await driver.execute('mobile: touchAndHold', {duration: 100, x: 100, y: 100});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/touchAndHold');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['duration', 'x', 'y']);
+      });
+    });
+
+    describe('tap', () => {
+      const commandName = 'tap';
+
+      it('should throw an error if no mandatory parameter is specified', async () => {
+        await driver.execute(`mobile: ${commandName}`, {}).should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {x: 100}).should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {y: 200}).should.be.rejectedWith(/parameter is mandatory/);
+      });
+
+      it('should throw an error if param is invalid', async () => {
+        await driver.execute(`mobile: ${commandName}`, {x: '', y: 1}).should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {x: 1, y: null}).should.be.rejectedWith(/should be a valid number/);
+      });
+
+      it('should proxy a tap request for an element through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4, x: 100, y: 100});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/tap/4');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['x', 'y']);
+      });
+
+      it('should proxy a tap request for a coordinate point through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {x: 100, y: 100});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/tap/0');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['x', 'y']);
+      });
+    });
+
+    describe('dragFromToForDuration', () => {
+      const commandName = 'dragFromToForDuration';
+
+      it('should throw an error if no mandatory parameter is specified', async () => {
+        await driver.execute(`mobile: ${commandName}`, {fromX: 1, fromY: 1, toX: 100, toY: 100})
+          .should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromY: 1, toX: 100, toY: 100})
+          .should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: 1, toX: 100, toY: 100})
+          .should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: 1, fromY: 1, toY: 100})
+          .should.be.rejectedWith(/parameter is mandatory/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: 1, fromY: 1, toX: 100})
+          .should.be.rejectedWith(/parameter is mandatory/);
+      });
+
+      it('should throw an error if param is invalid', async () => {
+        await driver.execute(`mobile: ${commandName}`, {duration: '', fromX: 1, fromY: 1, toX: 100, toY: 100})
+          .should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: '', fromY: 1, toX: 100, toY: 100})
+          .should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: 1, fromY: null, toX: 100, toY: 100})
+          .should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: 1, fromY: 1, toX: 'blabla', toY: 100})
+          .should.be.rejectedWith(/should be a valid number/);
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: 1, fromY: 1, toX: 100, toY: NaN})
+          .should.be.rejectedWith(/should be a valid number/);
+      });
+
+      it('should proxy a dragFromToForDuration request for an element through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {element: 4, duration: 100, fromX: 1, fromY: 1, toX: 100, toY: 100});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/element/4/dragfromtoforduration');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['duration', 'fromX', 'fromY', 'toX', 'toY']);
+      });
+
+      it('should proxy a dragFromToForDuration request for a coordinate point through to WDA', async () => {
+        await driver.execute(`mobile: ${commandName}`, {duration: 100, fromX: 1, fromY: 1, toX: 100, toY: 100});
+        proxySpy.calledOnce.should.be.true;
+        proxySpy.firstCall.args[0].should.eql('/wda/dragfromtoforduration');
+        proxySpy.firstCall.args[1].should.eql('POST');
+        proxySpy.firstCall.args[2].should.have.keys(['duration', 'fromX', 'fromY', 'toX', 'toY']);
       });
     });
   });
