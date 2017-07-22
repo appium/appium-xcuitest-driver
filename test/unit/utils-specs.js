46fd11e0540fb7b22696326ec97deb6964da838e
Mykola Mokhnach <nick+1@wire.com>
2017-07-22 15:37:24 +0200

    Adjust permissions for the current derived data folder only

--- test/unit/utils-specs.js
+++ test/unit/utils-specs.js
@@ -1,32 +1,58 @@
-import { clearSystemFiles, translateDeviceName } from '../../lib/utils';
+import { clearSystemFiles, translateDeviceName, adjustWDAAttachmentsPermissions } from '../../lib/utils';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { withMocks } from 'appium-test-support';
 import { utils as iosUtils } from 'appium-ios-driver';
+import { fs } from 'appium-support';
 
 
 chai.should();
 chai.use(chaiAsPromised);
 
 describe('utils', () => {
+  const DERIVED_DATA_ROOT = '/path/to/DerivedData/WebDriverAgent-eoyoecqmiqfeodgstkwbxkfyagll';
   describe('clearSystemFiles', withMocks({iosUtils}, (mocks) => {
     it('should delete logs', async () => {
-      let wda = {
-        derivedDataPath: '/path/to/DerivedData/WebDriverAgent-eoyoecqmiqfeodgstkwbxkfyagll',
-      };
+      let wda = {};
+      wda.retrieveDerivedDataPath = () => DERIVED_DATA_ROOT;
       mocks.iosUtils.expects('clearLogs')
         .once()
-        .withExactArgs([`${wda.derivedDataPath}/Logs`])
+        .withExactArgs([`${DERIVED_DATA_ROOT}/Logs`])
         .returns();
       await clearSystemFiles(wda);
       mocks.iosUtils.verify();
     });
     it('should do nothing if no derived data path is found', async () => {
       let wda = {};
+      wda.retrieveDerivedDataPath = () => null;
       mocks.iosUtils.expects('clearLogs')
         .never();
       await clearSystemFiles(wda);
       mocks.iosUtils.verify();
+    });
+  }));
+  describe('adjustWDAAttachmentsPermissions', withMocks({fs}, (mocks) => {
+    it('should change permissions to Attachments folder', async () => {
+      let wda = {};
+      wda.retrieveDerivedDataPath = () => DERIVED_DATA_ROOT;
+      mocks.fs.expects('exists')
+        .once()
+        .withExactArgs(`${DERIVED_DATA_ROOT}/Logs/Test/Attachments`)
+        .returns(true);
+      mocks.fs.expects('chmod')
+        .once()
+        .withExactArgs(`${DERIVED_DATA_ROOT}/Logs/Test/Attachments`, '555')
+        .returns();
+      await adjustWDAAttachmentsPermissions(wda, '555');
+      mocks.fs.verify();
+    });
+    it('should do nothing if no derived data path is found', async () => {
+      let wda = {};
+      wda.retrieveDerivedDataPath = () => null;
+      mocks.fs.expects('exists').never();
+      mocks.fs.expects('chmod').never();
+      await adjustWDAAttachmentsPermissions(wda, '777');
+      mocks.fs.verify();
     });
   }));
   describe('determineDevice', () => {
