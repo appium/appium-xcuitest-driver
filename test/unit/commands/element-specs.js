98cecb551a43b1ea9ea8f085e5ad7013fec03a8e
Jonathan Lipps <jlipps@gmail.com>
2017-07-28 11:18:49 -0700

    address review comments and add unit tests

--- test/unit/commands/element-specs.js
+++ test/unit/commands/element-specs.js
@@ -107,4 +107,48 @@ describe('element commands', function () {
       });
     });
   });
+
+  describe('getLocation for web elements', () => {
+    let driver = new XCUITestDriver();
+    const oldContext = driver.curContext;
+    const webEl = {ELEMENT: '5000'};
+    const fixtureXOffset = 100, fixtureYOffset = 200;
+    let executeStub = sinon.stub(driver, 'execute');
+    executeStub.returns([fixtureXOffset, fixtureYOffset]);
+    let atomsElStub = sinon.stub(driver, 'useAtomsElement', (el) => el);
+    let atomStub = sinon.stub(driver, 'executeAtom');
+    let proxyStub = sinon.stub(driver, 'proxyCommand');
+    atomStub.returns({x: 0, y: 0});
+
+    beforeEach(() => {
+      driver.curContext = "fake web context";
+    });
+
+    afterEach(() => {
+      driver.curContext = oldContext;
+      executeStub.reset();
+      atomsElStub.reset();
+      atomStub.reset();
+      proxyStub.reset();
+    });
+
+    it('should get location relative to scroll by default', async () => {
+      const loc = await driver.getLocation(webEl);
+      executeStub.calledOnce.should.be.false;
+      atomStub.calledOnce.should.be.true;
+      atomStub.firstCall.args[0].should.eql('get_top_left_coordinates');
+      loc.x.should.equal(0);
+      loc.y.should.equal(0);
+    });
+
+    it('should get location relative to document with abosluteWebLocations cap', async () => {
+      driver.opts.absoluteWebLocations = true;
+      const loc = await driver.getLocation(webEl);
+      executeStub.calledOnce.should.be.true;
+      atomStub.calledOnce.should.be.true;
+      atomStub.firstCall.args[0].should.eql('get_top_left_coordinates');
+      loc.x.should.equal(fixtureXOffset);
+      loc.y.should.equal(fixtureYOffset);
+    });
+  });
 });
