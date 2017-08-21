ffb23dba69298d8ddf29995ecd89425e1fffbe89
Mykola Mokhnach <nick+1@wire.com>
2017-08-21 14:31:36 +0200

    Add the necessary stuff to support pasteboard on iOS simulator (#508)
    * Add the necessary stuff to support pasteboard on iOS simulator
    
    * Add pasteboard extensions to index
    
    * Address review comments
    
    * Eliminate extra line
    
    * Remove unused stub

---
+++ test/unit/commands/pasteboard-specs.js
@@ -0,0 +1,61 @@
+import sinon from 'sinon';
+import XCUITestDriver from '../../..';
+
+const simctlModule = require('node-simctl');
+
+
+describe('pasteboard commands', function () {
+  const driver = new XCUITestDriver();
+  let isSimulatorSpy, setPasteboardSpy, getPasteboardSpy;
+
+  beforeEach(() => {
+    isSimulatorSpy = sinon.stub(driver, 'isSimulator');
+    setPasteboardSpy = sinon.stub(simctlModule, 'setPasteboard');
+    getPasteboardSpy = sinon.stub(simctlModule, 'getPasteboard');
+  });
+
+  afterEach(() => {
+    isSimulatorSpy.restore();
+    setPasteboardSpy.restore();
+    getPasteboardSpy.restore();
+  });
+
+  it('setPasteboard should not be called on a real device', async function () {
+    isSimulatorSpy.returns(false);
+    await driver.mobileSetPasteboard({content: 'bla'}).should.eventually.be.rejectedWith(/not supported/);
+    setPasteboardSpy.notCalled.should.be.true;
+  });
+
+  it('setPasteboard should fail if no content is provided', async function () {
+    isSimulatorSpy.returns(true);
+    await driver.mobileSetPasteboard().should.eventually.be.rejectedWith(/mandatory to set/);
+    setPasteboardSpy.notCalled.should.be.true;
+  });
+
+  it('getPasteboard should not be called on a real device', async function () {
+    isSimulatorSpy.returns(false);
+    await driver.mobileGetPasteboard().should.eventually.be.rejectedWith(/not supported/);
+    getPasteboardSpy.notCalled.should.be.true;
+  });
+
+  it('setPasteboard should invoke correct simctl method', async function () {
+    isSimulatorSpy.returns(true);
+    const opts = {
+      content: 'bla',
+      encoding: 'latin-1',
+    };
+    await driver.mobileSetPasteboard(opts);
+    setPasteboardSpy.calledOnce.should.be.true;
+    setPasteboardSpy.firstCall.args[0].should.eql(opts.content);
+    setPasteboardSpy.firstCall.args[1].should.eql(opts.encoding);
+  });
+
+  it('getPasteboard should invoke correct simctl method', async function () {
+    isSimulatorSpy.returns(true);
+    const content = 'bla';
+    getPasteboardSpy.returns(content);
+    const result = await driver.mobileGetPasteboard();
+    getPasteboardSpy.calledOnce.should.be.true;
+    result.should.eql(content);
+  });
+});
