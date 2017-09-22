2415cd06c1e2d479e7759c6b07b596c27b8799ef
Dan Graham <dan@saucelabs.com>
2017-09-22 12:43:48 -0700

    Fix: Typo was causing startCapture to be called more than once (#532)
    * Replaced `this.logs = this.log || {}` with `this.logs = this.logs || {}`
    * This was causing it to be overwritten every time
    * Was able to reproduce with a unit test that showed startCapture being called more than once

---
+++ test/unit/log-specs.js
@@ -0,0 +1,48 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import _ from 'lodash';
+import sinon from 'sinon';
+import * as IOSDriver from 'appium-ios-driver';
+import { startLogCapture } from '../../lib/commands/log';
+
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('XCUITestDriver - startLogCapture', function () {
+
+  let startCaptureSpy, crashLogStub, iosLogStub;
+
+  before(function () {
+    let spy = {
+      startCapture: _.noop,
+    };
+    startCaptureSpy = sinon.spy(spy, 'startCapture');
+    crashLogStub = sinon.stub(IOSDriver, 'IOSCrashLog', function () {
+      this.startCapture = _.noop;
+    });
+    iosLogStub = sinon.stub(IOSDriver, 'IOSLog', function () {
+      this.startCapture = spy.startCapture;
+    });
+  });
+
+  after(function () {
+    startCaptureSpy.restore();
+    crashLogStub.restore();
+    iosLogStub.restore();
+  });
+
+  // establish that the basic things work as we imagine
+  it('should not spawn more than one instance of idevicesyslog', async () => {
+    const fakeInstance = {
+      logs: undefined,
+      opts: {},
+      isRealDevice: _.noop,
+    };
+    startCaptureSpy.callCount.should.equal(0);
+    await startLogCapture.call(fakeInstance);
+    startCaptureSpy.callCount.should.equal(1);
+    await startLogCapture.call(fakeInstance);
+    startCaptureSpy.callCount.should.equal(1);
+  });
+});
