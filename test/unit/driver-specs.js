98182568540d2ba66cc2789c95ff27ab15f68270
Mykola Mokhnach <nick+1@wire.com>
2017-10-24 20:12:15 +0200

    Update unit tests

--- test/unit/driver-specs.js
+++ test/unit/driver-specs.js
@@ -82,6 +82,7 @@ describe('driver commands', () => {
       sandbox.stub(d, "startWda", anoop);
       sandbox.stub(d, "extractBundleId", anoop);
       sandbox.stub(d, "installApp", anoop);
+      sandbox.stub(d, "clearCaches", anoop);
       sandbox.stub(iosSettings, "setLocale", anoop);
       sandbox.stub(iosSettings, "setPreferences", anoop);
       sandbox.stub(xcode, "getMaxIOSSDK", async () => {
