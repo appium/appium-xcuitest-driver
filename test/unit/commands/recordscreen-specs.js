949980fc59ccbbe450075aa5bb3f64cfe967f694
Mykola Mokhnach <nick+1@wire.com>
2018-01-18 18:14:28 +0100

    Tune the logic for Simulator recorder

--- test/unit/commands/recordscreen-specs.js
+++ test/unit/commands/recordscreen-specs.js
@@ -26,9 +26,6 @@ describe('basic', withMocks({driver, fs, tempDir, utils, teen_process}, function
   describe('startRecordingScreen', function () {
     beforeEach(function () {
       driver._recentScreenRecordingPath = null;
-      mocks.driver.expects('isRealDevice').atLeast(1).returns(false);
-      mocks.utils.expects('getPidUsingPattern')
-        .atLeast(1).withExactArgs(`simctl io ${udid} recordVideo`).returns(null);
     });
     afterEach(function () {
       mocks.driver.verify();
@@ -38,19 +35,25 @@ describe('basic', withMocks({driver, fs, tempDir, utils, teen_process}, function
       mocks.fs.verify();
     });
 
-    it('should call simctl to start screen recording', async function () {
+    it('should call simctl to start screen recording on Simulator', async function () {
+      mocks.driver.expects('isRealDevice').atLeast(1).returns(false);
+      mocks.utils.expects('getPidUsingPattern')
+        .atLeast(1).withExactArgs(`simctl io ${udid} recordVideo`).returns(null);
       mocks.teen_process.expects('exec').once().returns(new B(() => {}));
-      mocks.fs.expects('stat').once().returns({size: 39571});
 
       await driver.startRecordingScreen();
       driver._recentScreenRecordingPath.should.not.be.empty;
     });
 
-    it('should return previous capture before starting a new recording', async function () {
+    it('should return previous capture before starting a new recording on Simulator', async function () {
       const previousPath = '/some/video.mp4';
 
+      mocks.driver.expects('isRealDevice').atLeast(1).returns(false);
+      mocks.utils.expects('getPidUsingPattern')
+        .atLeast(1).withExactArgs(`simctl io ${udid} recordVideo`).returns(null);
       mocks.teen_process.expects('exec').returns(new B(() => {}));
-      mocks.fs.expects('stat').atLeast(1).returns({size: 39571});
+      mocks.fs.expects('exists').once().withExactArgs(previousPath).returns(true);
+      mocks.fs.expects('stat').once().returns({size: 39571});
       mocks.fs.expects('readFile').once().withExactArgs(previousPath).returns(mediaContent);
       mocks.fs.expects('rimraf').once().withExactArgs(previousPath);
       mocks.tempDir.expects('path').once().returns(localFile);
@@ -61,7 +64,11 @@ describe('basic', withMocks({driver, fs, tempDir, utils, teen_process}, function
       driver._recentScreenRecordingPath.should.be.eql(localFile);
     });
 
-    it('should call stat multiple times until size is big enough', async function () {
+    it('should call stat multiple times until size is big enough on a real device', async function () {
+      mocks.driver.expects('isRealDevice').atLeast(1).returns(true);
+      mocks.utils.expects('getPidUsingPattern')
+        .atLeast(1).withExactArgs(`xrecord.*${udid}`).returns(null);
+      mocks.fs.expects('which').once().withExactArgs('xrecord').returns('xrecord');
       mocks.teen_process.expects('exec').returns(new B(() => {}));
       let fileSizeStub = sinon.stub(fs, 'stat');
       try {
@@ -99,6 +106,8 @@ describe('basic', withMocks({driver, fs, tempDir, utils, teen_process}, function
       xcrun simctl io ${udid} recordVideo ${localFile}
       `});
       mocks.teen_process.expects('exec').withExactArgs('kill', ['-2', pid]);
+      mocks.teen_process.expects('exec').withExactArgs('kill', ['-0', pid]).throws();
+      mocks.fs.expects('exists').once().withExactArgs(localFile).returns(true);
       mocks.fs.expects('readFile').once().withExactArgs(localFile).returns(mediaContent);
       mocks.fs.expects('rimraf').once().withExactArgs(localFile);
       mocks.fs.expects('stat').once().withExactArgs(localFile).returns({size: 100});
@@ -111,6 +120,8 @@ describe('basic', withMocks({driver, fs, tempDir, utils, teen_process}, function
       driver._recentScreenRecordingPath = localFile;
       mocks.utils.expects('getPidUsingPattern').atLeast(1).returns(pid);
       mocks.teen_process.expects('exec').withExactArgs('kill', ['-2', pid]);
+      mocks.teen_process.expects('exec').withExactArgs('kill', ['-0', pid]).throws();
+      mocks.fs.expects('exists').once().withExactArgs(localFile).returns(true);
       mocks.fs.expects('readFile').once().withExactArgs(localFile).returns(mediaContent);
       mocks.fs.expects('rimraf').once().withExactArgs(localFile);
       mocks.fs.expects('stat').once().withExactArgs(localFile).returns({size: 100});
@@ -121,6 +132,7 @@ describe('basic', withMocks({driver, fs, tempDir, utils, teen_process}, function
     it('should fail if the recorded file is too large', async function () {
       driver._recentScreenRecordingPath = localFile;
       mocks.utils.expects('getPidUsingPattern').atLeast(1).returns(null);
+      mocks.fs.expects('exists').once().withExactArgs(localFile).returns(true);
       mocks.fs.expects('rimraf').once().withExactArgs(localFile);
       mocks.fs.expects('stat').once().withExactArgs(localFile)
         .returns({size: process.memoryUsage().heapTotal});
