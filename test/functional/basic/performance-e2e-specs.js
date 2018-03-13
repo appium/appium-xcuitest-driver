bc58f0ca7c2dac356fbbbdec3cf7ff78ee19ba90
Mykola Mokhnach <nick+1@wire.com>
2018-03-13 20:00:43 +0100

    Implement performance measurement with `instruments` utility (#637)
    * Implement performance measurement with `instruments` utility
    
    * Fix the comment

---
+++ test/functional/basic/performance-e2e-specs.js
@@ -0,0 +1,35 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import B from 'bluebird';
+import { UICATALOG_CAPS } from '../desired';
+import { initSession, deleteSession, MOCHA_TIMEOUT } from '../helpers/session';
+
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('XCUITestDriver - perfomance', function () {
+  this.timeout(MOCHA_TIMEOUT);
+
+  let driver;
+
+  describe('record performance metrics', function () {
+    before(async function () {
+      driver = await initSession(UICATALOG_CAPS);
+      driver.relaxedSecurityEnabled = true;
+    });
+    after(async function () {
+      await deleteSession();
+    });
+
+    it('should return recorded trace file on stop', async function () {
+      if (process.env.CI) {
+        // It looks like Travis is just way too slow
+        return this.skip();
+      }
+      await driver.execute('mobile: startPerfRecord', {});
+      await B.delay(5000);
+      (await driver.execute('mobile: stopPerfRecord', {})).should.not.be.empty;
+    });
+  });
+});
