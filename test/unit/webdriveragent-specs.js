c6e38c9ae9b757cf3358d681e3b761bd762f707e
Bruno Alassia <vruno.alassia@gmail.com>
2018-05-16 18:21:25 -0300

    Add derivedDataPath capability (#669)
    * add new capability for setting the derivedDataPath for xcodebuild
    
    * use -derivedDataPath
    
    * use -derivedDataPath
    
    * add derivedDataPath to wda.js
    
    * add unit test
    
    * add unit test
    
    * fix -derivedDataPath value
    
    * add functional test for derivedDataPath
    
    * update readme
    
    * update readme
    
    * remove log
    
    * update readme
    
    * remove unused wda.derivedDataPath
    
    * dont skip derivedDataPath test
    
    * rollback to use 10.3
    
    * update readme
    
    * skip test for now

--- test/unit/webdriveragent-specs.js
+++ test/unit/webdriveragent-specs.js
@@ -20,6 +20,7 @@ const fakeConstructorArgs = {
 const defaultAgentPath = path.resolve(BOOTSTRAP_PATH, 'WebDriverAgent.xcodeproj');
 const customBootstrapPath = '/path/to/wda';
 const customAgentPath = '/path/to/some/agent/WebDriverAgent.xcodeproj';
+const customDerivedDataPath = '/path/to/some/agent/DerivedData/';
 
 describe('Constructor', function () {
   it('should have a default wda agent if not specified', function () {
@@ -41,6 +42,12 @@ describe('Constructor', function () {
     }, fakeConstructorArgs));
     agent.bootstrapPath.should.eql(customBootstrapPath);
     agent.agentPath.should.eql(customAgentPath);
+  });
+  it('should have custom derivedDataPath if specified', function () {
+    let agent = new WebDriverAgent({}, _.defaults({
+      derivedDataPath: customDerivedDataPath
+    }, fakeConstructorArgs));
+    agent.xcodebuild._derivedDataPath.should.eql(customDerivedDataPath);
   });
 });
 
