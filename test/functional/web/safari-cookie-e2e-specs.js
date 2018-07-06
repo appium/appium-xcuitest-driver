07fcc50c663d47655353a4de0370ded287c174a9
Isaac Murchie <isaac@saucelabs.com>
2018-07-06 14:46:54 -0700

    Fix for secure cookies, and add tests

--- test/functional/web/safari-cookie-e2e-specs.js
+++ test/functional/web/safari-cookie-e2e-specs.js
@@ -9,6 +9,28 @@ import { GUINEA_PIG_PAGE, GUINEA_PIG_IFRAME_PAGE } from './helpers';
 chai.should();
 chai.use(chaiAsPromised);
 
+const doesIncludeCookie = function (cookies, cookie) {
+  cookies.map((c) => c.name).should.include(cookie.name);
+  cookies.map((c) => c.value).should.include(cookie.value);
+};
+const doesNotIncludeCookie = function (cookies, cookie) {
+  cookies.map((c) => c.name).should.not.include(cookie.name);
+  cookies.map((c) => c.value).should.not.include(cookie.value);
+};
+
+const newCookie = {
+  name: 'newcookie',
+  value: 'i am new here'
+};
+const oldCookie1 = {
+  name: 'guineacookie1',
+  value: 'i am a cookie value'
+};
+const oldCookie2 = {
+  name: 'guineacookie2',
+  value: 'cookié2'
+};
+
 describe('safari - cookies', function () {
   this.timeout(MOCHA_TIMEOUT);
 
@@ -36,104 +58,83 @@ describe('safari - cookies', function () {
   });
 
   describe('within webview', function () {
-    const newCookie = {
-      name: 'newcookie',
-      value: 'i am new here'
-    };
-    const oldCookie1 = {
-      name: 'guineacookie1',
-      value: 'i am a cookie value'
-    };
-    const oldCookie2 = {
-      name: 'guineacookie2',
-      value: 'cookié2'
-    };
+    describe('insecure', function () {
+      beforeEach(async function () {
+        await driver.get(GUINEA_PIG_PAGE);
+        await driver.deleteCookie(newCookie.name);
+      });
 
-    let doesIncludeCookie = function (cookies, cookie) {
-      cookies.map((c) => c.name).should.include(cookie.name);
-      cookies.map((c) => c.value).should.include(cookie.value);
-    };
-    let doesNotIncludeCookie = function (cookies, cookie) {
-      cookies.map((c) => c.name).should.not.include(cookie.name);
-      cookies.map((c) => c.value).should.not.include(cookie.value);
-    };
+      it('should be able to get cookies for a page', async function () {
+        let cookies = await driver.allCookies();
+        cookies.length.should.equal(2);
+        doesIncludeCookie(cookies, oldCookie1);
+        doesIncludeCookie(cookies, oldCookie2);
+      });
 
-    beforeEach(async function () {
-      await driver.get(GUINEA_PIG_PAGE);
+      it('should be able to set a cookie for a page', async function () {
+        await driver.setCookie(newCookie);
+        const cookies = await driver.allCookies();
+        doesIncludeCookie(cookies, newCookie);
+
+        // should not clobber old cookies
+        doesIncludeCookie(cookies, oldCookie1);
+        doesIncludeCookie(cookies, oldCookie2);
+      });
+
+      it('should be able to set a cookie with expiry', async function () {
+        const expiredCookie = Object.assign({}, newCookie, {
+          expiry: parseInt(Date.now() / 1000, 10) - 1000, // set cookie in past
+          name: 'expiredcookie',
+        });
+
+        let cookies = await driver.allCookies();
+        doesNotIncludeCookie(cookies, expiredCookie);
+
+        await driver.setCookie(expiredCookie);
+        cookies = await driver.allCookies();
+        // should not include cookie we just added because of expiry
+        doesNotIncludeCookie(cookies, expiredCookie);
+
+        // should not clobber old cookies
+        doesIncludeCookie(cookies, oldCookie1);
+        doesIncludeCookie(cookies, oldCookie2);
+
+        await driver.deleteCookie(expiredCookie.name);
+      });
+
+      it('should be able to delete one cookie', async function () {
+        await driver.setCookie(newCookie);
+        let cookies = await driver.allCookies();
+        doesIncludeCookie(cookies, newCookie);
+
+        await driver.deleteCookie(newCookie.name);
+        cookies = await driver.allCookies();
+        doesNotIncludeCookie(cookies, newCookie);
+
+        doesIncludeCookie(cookies, oldCookie1);
+        doesIncludeCookie(cookies, oldCookie2);
+      });
+
+      it('should be able to delete all cookies', async function () {
+        await driver.setCookie(newCookie);
+        let cookies = await driver.allCookies();
+        doesIncludeCookie(cookies, newCookie);
+
+        await driver.deleteAllCookies();
+        cookies = await driver.allCookies();
+        cookies.length.should.equal(0);
+
+        doesNotIncludeCookie(cookies, oldCookie1);
+        doesNotIncludeCookie(cookies, oldCookie2);
+      });
     });
 
-    it('should be able to get cookies for a page', async function () {
-      let cookies = await driver.allCookies();
-      cookies.length.should.equal(2);
-      doesIncludeCookie(cookies, oldCookie1);
-      doesIncludeCookie(cookies, oldCookie2);
-    });
-
-    it('should be able to set a cookie for a page', async function () {
-      await driver.deleteCookie(newCookie.name);
-      let cookies = await driver.allCookies();
-      doesNotIncludeCookie(cookies, newCookie);
-
-      await driver.setCookie(newCookie);
-      cookies = await driver.allCookies();
-      doesIncludeCookie(cookies, newCookie);
-
-      // should not clobber old cookies
-      doesIncludeCookie(cookies, oldCookie1);
-      doesIncludeCookie(cookies, oldCookie2);
-    });
-
-    it('should be able to set a cookie with expiry', async function () {
-      let expiredCookie = _.defaults({
-        expiry: parseInt(Date.now() / 1000, 10) - 1000 // set cookie in past
-      }, newCookie);
-
-      await driver.deleteCookie(expiredCookie.name);
-      let cookies = await driver.allCookies();
-      doesNotIncludeCookie(cookies, expiredCookie);
-
-      await driver.setCookie(expiredCookie);
-      cookies = await driver.allCookies();
-      // should not include cookie we just added because of expiry
-      doesNotIncludeCookie(cookies, expiredCookie);
-
-      // should not clobber old cookies
-      doesIncludeCookie(cookies, oldCookie1);
-      doesIncludeCookie(cookies, oldCookie2);
-    });
-
-    it('should be able to delete one cookie', async function () {
-      await driver.deleteCookie(newCookie.name);
-      let cookies = await driver.allCookies();
-      doesNotIncludeCookie(cookies, newCookie);
-
-      await driver.setCookie(newCookie);
-      cookies = await driver.allCookies();
-      doesIncludeCookie(cookies, newCookie);
-
-      await driver.deleteCookie(newCookie.name);
-      cookies = await driver.allCookies();
-      doesNotIncludeCookie(cookies, newCookie);
-
-      doesIncludeCookie(cookies, oldCookie1);
-      doesIncludeCookie(cookies, oldCookie2);
-    });
-
-    it('should be able to delete all cookies', async function () {
-      await driver.deleteCookie(newCookie.name);
-      let cookies = await driver.allCookies();
-      doesNotIncludeCookie(cookies, newCookie);
-
-      await driver.setCookie(newCookie);
-      cookies = await driver.allCookies();
-      doesIncludeCookie(cookies, newCookie);
-
-      await driver.deleteAllCookies();
-      cookies = await driver.allCookies();
-      cookies.length.should.equal(0);
-
-      doesNotIncludeCookie(cookies, oldCookie1);
-      doesNotIncludeCookie(cookies, oldCookie2);
+    describe('secure', function () {
+      /*
+       * secure cookie tests are in `./safari-ssl-e2e-specs.js`
+       */
     });
   });
 });
+
+export { doesIncludeCookie, doesNotIncludeCookie, newCookie, oldCookie1, oldCookie2 };
