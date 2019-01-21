2d4ab558369de54b7e607a5e3f599896db4db94a
Isaac Murchie <isaac.appium@gmail.com>
2019-01-21 13:59:58 -0500

    Add projectRoot to gulpfile boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-xcuitest-driver',
+  projectRoot: __dirname,
   coverage: {
     files: ['./build/test/unit/**/*-specs.js', '!./build/test/functional/**'],
     verbose: false
