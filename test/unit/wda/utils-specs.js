f8759c6dacac9e3331c07b9606f948e38abed745
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-03-18 17:41:27 +0900

    fix xctestrun path

---
+++ test/unit/wda/utils-specs.js
@@ -0,0 +1,88 @@
+import { getXctestrunFilePath } from '../../../lib/wda/utils';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { withMocks } from 'appium-test-support';
+import { fs } from 'appium-support';
+import path from 'path';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('utils', function () {
+  describe('#getXctestrunFilePath', withMocks({fs}, function (mocks) {
+    const platformVersion = '12.0';
+    const sdkVersion = '12.2';
+    const udid = 'xxxxxyyyyyyzzzzzz';
+    const bootstrapPath = 'path/to/data';
+
+    afterEach(function () {
+      mocks.verify();
+    });
+
+    it('should return sdk based path with udid', async function () {
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/${udid}_${sdkVersion}.xctestrun`))
+        .returns(true);
+      mocks.fs.expects('copyFile')
+        .never();
+      await getXctestrunFilePath(true, udid, platformVersion, sdkVersion, bootstrapPath)
+        .should.eventually.equal(path.resolve(`${bootstrapPath}/${udid}_${sdkVersion}.xctestrun`));
+    });
+
+    it('should return sdk based path without udid, copy them', async function () {
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/${udid}_${sdkVersion}.xctestrun`))
+        .returns(false);
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/WebDriverAgentRunner_iphoneos${sdkVersion}-arm64.xctestrun`))
+        .returns(true);
+      mocks.fs.expects('copyFile')
+        .withExactArgs(
+          path.resolve(`${bootstrapPath}/WebDriverAgentRunner_iphoneos${sdkVersion}-arm64.xctestrun`),
+          path.resolve(`${bootstrapPath}/${udid}_${sdkVersion}.xctestrun`)
+        )
+        .returns(true);
+      await getXctestrunFilePath(true, udid, platformVersion, sdkVersion, bootstrapPath)
+        .should.eventually.equal(path.resolve(`${bootstrapPath}/${udid}_${sdkVersion}.xctestrun`));
+    });
+
+    it('should return platform based path', async function () {
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/${udid}_${sdkVersion}.xctestrun`))
+        .returns(false);
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/WebDriverAgentRunner_iphonesimulator${sdkVersion}-x86_64.xctestrun`))
+        .returns(false);
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/${udid}_${platformVersion}.xctestrun`))
+        .returns(true);
+      mocks.fs.expects('copyFile')
+        .never();
+      await getXctestrunFilePath(false, udid, platformVersion, sdkVersion, bootstrapPath)
+        .should.eventually.equal(path.resolve(`${bootstrapPath}/${udid}_${platformVersion}.xctestrun`));
+    });
+
+    it('should return platform based path without udid, copy them', async function () {
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/${udid}_${sdkVersion}.xctestrun`))
+        .returns(false);
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/WebDriverAgentRunner_iphonesimulator${sdkVersion}-x86_64.xctestrun`))
+        .returns(false);
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/${udid}_${platformVersion}.xctestrun`))
+        .returns(false);
+      mocks.fs.expects('exists')
+        .withExactArgs(path.resolve(`${bootstrapPath}/WebDriverAgentRunner_iphonesimulator${platformVersion}-x86_64.xctestrun`))
+        .returns(true);
+      mocks.fs.expects('copyFile')
+        .withExactArgs(
+          path.resolve(`${bootstrapPath}/WebDriverAgentRunner_iphonesimulator${platformVersion}-x86_64.xctestrun`),
+          path.resolve(`${bootstrapPath}/${udid}_${platformVersion}.xctestrun`)
+        )
+        .returns(true);
+      await getXctestrunFilePath(false, udid, platformVersion, sdkVersion, bootstrapPath)
+        .should.eventually.equal(path.resolve(`${bootstrapPath}/${udid}_${platformVersion}.xctestrun`));
+    });
+  }));
+});
