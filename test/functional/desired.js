3c4cc23d562d539f4221737b72cc27c21c38b80c
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-04-04 21:40:54 +0900

    try to increase timeout

--- test/functional/desired.js
+++ test/functional/desired.js
@@ -50,6 +50,8 @@ let GENERIC_CAPS = {
   maxTypingFrequency: 30,
   clearSystemFiles: true,
   showXcodeLog: SHOW_XCODE_LOG,
+  wdaLaunchTimeout: (60 * 1000 * 4),
+  wdaConnectionTimeout: (60 * 1000 * 8)
 };
 
 if (process.env.CLOUD) {
