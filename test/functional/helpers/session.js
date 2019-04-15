acf4ce32096003c7ea3173132509e6c3b8dd30f0
Isaac A. Murchie <isaac.appium@gmail.com>
2019-04-15 16:24:46 -0400

    Fix Travis build
    * Get rid of timeout on Travis mocha
    
    * Get WDA deps before running, so it does not get done in the first session
    
    * Fix 12.2 platformVersion
    
    * Remove superfluous xcode specification

--- test/functional/helpers/session.js
+++ test/functional/helpers/session.js
@@ -34,7 +34,7 @@ function getHost () {
 const HOST = getHost();
 const PORT = getPort();
 // on CI the timeout needs to be long, mostly so WDA can be built the first time
-const MOCHA_TIMEOUT = 60 * 1000 * (process.env.CI ? 40 : 4);
+const MOCHA_TIMEOUT = 60 * 1000 * (process.env.CI ? 0 : 4);
 const WDA_PORT = 8200;
 
 let driver, server;
