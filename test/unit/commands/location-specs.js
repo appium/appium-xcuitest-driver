ccf0ee2f7516b76bb3c6f5af1961bac5a1b1e648
umutuzgur <umutuzgur@gmail.com>
2019-07-11 22:03:06 +0200

    feat: remove idevicelocation (#1006)

--- test/unit/commands/location-specs.js
+++ test/unit/commands/location-specs.js
@@ -1,11 +1,9 @@
 import sinon from 'sinon';
 import XCUITestDriver from '../../..';
-import { fs } from 'appium-support';
-import * as teenProcess from 'teen_process';
+import { services } from 'appium-ios-device';
 
 describe('location commands', function () {
   const udid = '1234';
-  const toolName = 'idevicelocation';
 
   const driver = new XCUITestDriver();
   const proxySpy = sinon.stub(driver, 'proxyCommand');
@@ -15,17 +13,19 @@ describe('location commands', function () {
   });
 
   describe('setLocation', function () {
-    let execStub;
-    let fsWhichStub;
+    let startSimulateLocationServiceStub;
+    let setLocationStub;
 
     beforeEach(function () {
-      execStub = sinon.stub(teenProcess, 'exec');
-      fsWhichStub = sinon.stub(fs, 'which');
+      startSimulateLocationServiceStub = sinon.stub(services, 'startSimulateLocationService');
+      let mockService = { setLocation () {}, close () {} };
+      setLocationStub = sinon.stub(mockService, 'setLocation');
+      startSimulateLocationServiceStub.returns(mockService);
     });
 
     afterEach(function () {
-      execStub.restore();
-      fsWhichStub.restore();
+      startSimulateLocationServiceStub.restore();
+      setLocationStub.restore();
     });
 
     it('should fail when location object is wrong', async function () {
@@ -38,30 +38,20 @@ describe('location commands', function () {
         driver.opts.realDevice = true;
       });
 
-      it('should use idevicelocation to set a location', async function () {
-        fsWhichStub.returns(toolName);
-        await driver.setGeoLocation({latitude: '1.234', longitude: '2.789'});
+      it('should use location service to set a location', async function () {
+        await driver.setGeoLocation({latitude: 1.234, longitude: 2.789});
 
-        execStub.calledOnce.should.be.true;
-        execStub.firstCall.args[0].should.eql(toolName);
-        execStub.firstCall.args[1].should.eql(['-u', udid, '1.234', '2.789']);
+        startSimulateLocationServiceStub.calledOnce.should.be.true;
+        startSimulateLocationServiceStub.firstCall.args[0].should.eql(udid);
+        setLocationStub.args[0].should.eql([1.234, 2.789]);
       });
 
-      it('should use idevicelocation to set a location with negative values', async function () {
-        fsWhichStub.returns(toolName);
+      it('should use location service to set a location with negative values', async function () {
         await driver.setGeoLocation({latitude: 1.234, longitude: -2});
 
-        execStub.calledOnce.should.be.true;
-        execStub.firstCall.args[0].should.eql(toolName);
-        execStub.firstCall.args[1].should.eql(['-u', udid, '1.234', '--', '-2']);
-      });
-
-      it('should fail when idevicelocation doesnt exist on the host', async function () {
-        fsWhichStub.throws();
-        await driver.setGeoLocation({
-          latitude: '1.234',
-          longitude: '2.789'}
-        ).should.be.rejectedWith(`idevicelocation doesn't exist on the host`);
+        startSimulateLocationServiceStub.calledOnce.should.be.true;
+        startSimulateLocationServiceStub.firstCall.args[0].should.eql(udid);
+        setLocationStub.args[0].should.eql([1.234, -2]);
       });
     });
 
