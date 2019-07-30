9fdefa844a426446eee3439726f4dedeba2ce196
Umut Uzgur <umutuzgur@gmail.com>
2019-07-30 13:23:27 +0200

    refactor: Remove idevicescreenshot since don't need it

--- test/unit/commands/screenshots-specs.js
+++ test/unit/commands/screenshots-specs.js
@@ -1,17 +1,13 @@
-import path from 'path';
 import sinon from 'sinon';
 import XCUITestDriver from '../../..';
-import { fs, tempDir } from 'appium-support';
 
 const simctlModule = require('node-simctl');
-const teenProcessModule = require('teen_process');
 
 describe('screenshots commands', function () {
   let driver;
   let proxyStub;
 
   const base64PortraitResponse = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==';
-  const base64LandscapeResponse = 'iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAD0lEQVR4AWNgqGeoZ0AGAA70AP/hygDpAAAAAElFTkSuQmCC';
 
   beforeEach(function () {
     driver = new XCUITestDriver();
@@ -68,109 +64,6 @@ describe('screenshots commands', function () {
         proxyStub.calledOnce.should.be.true;
         proxyStub.firstCall.args[0].should.eql('/screenshot');
         proxyStub.firstCall.args[1].should.eql('GET');
-      });
-
-      describe('idevicescreenshot', function () {
-        const pngPath = '/some/file.png';
-        const udid = '1234';
-        const toolName = 'idevicescreenshot';
-        const file = path.resolve(__dirname, '..', '..', '..', '..', 'test', 'assets', 'test.png');
-        let pngFileContent;
-
-        let fsExistsStub;
-        let fsWhichStub;
-        let fsRimRafStub;
-        let fsReadFileStub;
-        let execStub;
-        let pathStub;
-
-        before(async function () {
-          pngFileContent = await fs.readFile(file);
-        });
-
-        beforeEach(function () {
-          driver.opts.realDevice = true;
-          driver.opts.udid = udid;
-        });
-        afterEach(function () {
-          for (const stub of [fsExistsStub, fsWhichStub, fsReadFileStub, fsRimRafStub, execStub, pathStub]) {
-            if (stub) {
-              stub.restore();
-            }
-          }
-        });
-
-        describe('success', function () {
-          beforeEach(function () {
-            fsExistsStub = sinon.stub(fs, 'exists');
-            fsExistsStub.returns(true);
-
-            fsWhichStub = sinon.stub(fs, 'which');
-            fsWhichStub.returns(toolName);
-
-            fsRimRafStub = sinon.stub(fs, 'rimraf');
-
-            fsReadFileStub = sinon.stub(fs, 'readFile');
-            fsReadFileStub.returns(pngFileContent);
-
-            execStub = sinon.stub(teenProcessModule, 'exec');
-
-            pathStub = sinon.stub(tempDir, 'path');
-            pathStub.withArgs({prefix: `screenshot-${udid}`, suffix: '.png'}).returns(pngPath);
-          });
-          afterEach(function () {
-            fsWhichStub.calledOnce.should.be.true;
-            fsWhichStub.firstCall.args[0].should.eql(toolName);
-
-            execStub.calledOnce.should.be.true;
-            execStub.firstCall.args[0].should.eql(toolName);
-            execStub.firstCall.args[1].should.eql(['-u', udid, pngPath]);
-
-            fsRimRafStub.callCount.should.eql(2);
-
-            fsReadFileStub.calledOnce.should.be.true;
-            fsReadFileStub.firstCall.args[0].should.eql(pngPath);
-
-            pathStub.calledOnce.should.be.true;
-          });
-
-          it('should use idevicescreenshot if WDA fails', async function () {
-            proxyStub.onFirstCall().returns(null);
-            proxyStub.onSecondCall().returns('PORTRAIT');
-
-            (await driver.getScreenshot()).should.eql(pngFileContent.toString('base64'));
-
-            proxyStub.callCount.should.eql(2);
-            proxyStub.firstCall.args.should.eql(['/screenshot', 'GET']);
-            proxyStub.secondCall.args.should.eql(['/orientation', 'GET']);
-          });
-          it('should use idevicescreenshot if specified in realDeviceScreenshotter cap', async function () {
-            proxyStub.onFirstCall().returns('LANDSCAPE');
-            driver.opts.realDeviceScreenshotter = 'idevicescreenshot';
-
-            (await driver.getScreenshot()).should.eql(base64LandscapeResponse);
-
-            proxyStub.callCount.should.eql(1);
-          });
-        });
-        describe('failure', function () {
-          beforeEach(function () {
-
-            proxyStub.onFirstCall().returns(null);
-
-            fsWhichStub = sinon.stub(fs, 'which');
-            fsWhichStub.throws(new Error('No program found'));
-          });
-          afterEach(function () {
-            fsWhichStub.calledOnce.should.be.true;
-            fsWhichStub.firstCall.args[0].should.eql(toolName);
-          });
-
-          it('should throw an error if idevicescreenshot is not available and realDeviceScreenshotter set', async function () {
-            driver.opts.realDeviceScreenshotter = 'idevicescreenshot';
-            await driver.getScreenshot().should.eventually.be.rejectedWith(/No 'idevicescreenshot' program found/);
-          });
-        });
       });
     });
   });
