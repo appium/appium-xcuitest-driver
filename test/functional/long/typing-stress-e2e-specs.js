ad6da94920751eb8871a9566b838724a7618aec9
Isaac Murchie <isaac.appium@gmail.com>
2019-09-19 09:09:40 -0400

    fix: re-enable all tests

--- test/functional/long/typing-stress-e2e-specs.js
+++ test/functional/long/typing-stress-e2e-specs.js
@@ -9,9 +9,7 @@ chai.should();
 chai.use(chaiAsPromised);
 
 // leave the long test to Travis
-const TYPING_TRIES = process.env.CI
-  ? process.env.CLOUD ? 100 : 200
-  : 10;
+const TYPING_TRIES = process.env.CI ? 100 : 10;
 
 describe('XCUITestDriver - long tests', function () {
   this.timeout(0);
