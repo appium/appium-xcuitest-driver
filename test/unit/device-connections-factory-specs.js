e110cbfd14bf2c4919fb2f375d2b29517b36356b
Mykola Mokhnach <mokhnach@gmail.com>
2019-10-31 21:15:45 +0100

    fix: Make sure ports are closed properly after the cleanup (#1094)

--- test/unit/device-connections-factory-specs.js
+++ test/unit/device-connections-factory-specs.js
@@ -47,15 +47,15 @@ describe('DeviceConnectionsFactory', function () {
     devConFactory.listConnections(null, 23424).should.eql([]);
   });
 
-  it('should properly release proxied connections', function () {
+  it('should properly release proxied connections', async function () {
     devConFactory._connectionsMapping = {
       'udid:1234': {iproxy: {quit: () => {}}},
       'udid:5678': {},
       'udid4:6545': {iproxy: {quit: () => {}}},
     };
 
-    devConFactory._releaseProxiedConnections(_.keys(devConFactory._connectionsMapping))
-      .should.eql(['udid:1234', 'udid4:6545']);
+    await devConFactory._releaseProxiedConnections(_.keys(devConFactory._connectionsMapping))
+      .should.eventually.eql(['udid:1234', 'udid4:6545']);
   });
 
 });
