4046dbab2c26a192d062f5a7d78fd18650b9df84
Dan <dpgraham.85@gmail.com>
2020-11-22 17:44:58 -0800

    try big sur

--- test/functional/web/safari-ssl-e2e-specs.js
+++ test/functional/web/safari-ssl-e2e-specs.js
@@ -56,7 +56,7 @@ if (!process.env.REAL_DEVICE && !process.env.CLOUD) {
       }
     });
 
-    it('should open pages with untrusted certs if the cert was provided in desired capabilities', async function () {
+    it.only('should open pages with untrusted certs if the cert was provided in desired capabilities', async function () {
       try {
         driver = await initSession(caps);
         await driver.source().should.eventually.include('Arbitrary text');
