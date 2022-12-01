31d8edcab6aeb17c469f7b91282775ba328c7fee
Mykola Mokhnach <mokhnach@gmail.com>
2022-12-01 08:51:55 +0100

    Simplify

--- scripts/open-wda.js
+++ scripts/open-wda.js
@@ -1,25 +1,12 @@
-const fs = require('fs/promises');
 const path = require('path');
 const { exec } = require('teen_process');
 const log = require('fancy-log');
-const WDA_INDEX_PATH = require.resolve('appium-webdriveragent');
+const { BOOTSTRAP_PATH } = require('appium-webdriveragent');
 const XCODEPROJ_NAME = 'WebDriverAgent.xcodeproj';
 
 
-async function getXcodeprojPath () {
-  const pathParts = WDA_INDEX_PATH.split(path.sep).map((x) => x === '' ? path.sep : x);
-  for (let i = -1; i > -pathParts.length; i--) {
-    const currentPath = path.join(...pathParts.slice(-pathParts.length, i), XCODEPROJ_NAME);
-    try {
-      await fs.access(currentPath, fs.constants.R_OK);
-      return currentPath;
-    } catch (ign) {}
-  }
-  throw new Error(`Cannot find '${XCODEPROJ_NAME}' under '${WDA_INDEX_PATH}' hierarchy`);
-}
-
 async function openWda () {
-  const dstPath = await getXcodeprojPath();
+  const dstPath = path.resolve(BOOTSTRAP_PATH, XCODEPROJ_NAME);
   log.info(`Opening '${dstPath}' in Xcode`);
   await exec('open', [dstPath]);
 }
