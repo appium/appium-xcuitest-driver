e2255fe92d1a4e3bb129b840b06a61fc312d1a11
Mykola Mokhnach <mokhnach@gmail.com>
2022-12-01 21:07:43 +0100

    feat: Add a script to open WDA in Xcode (#1457)

---
+++ scripts/open-wda.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const { exec } = require('teen_process');
+const log = require('fancy-log');
+const { BOOTSTRAP_PATH } = require('appium-webdriveragent');
+const XCODEPROJ_NAME = 'WebDriverAgent.xcodeproj';
+
+
+async function openWda () {
+  const dstPath = path.resolve(BOOTSTRAP_PATH, XCODEPROJ_NAME);
+  log.info(`Opening '${dstPath}'`);
+  await exec('open', [dstPath]);
+}
+
+(async () => await openWda())();
