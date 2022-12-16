c9fc1dc7d70a2050435a651d77bc6e37253965cd
Mykola Mokhnach <mokhnach@gmail.com>
2022-12-16 15:09:33 +0100

    feat: Bump the major version of appium-ios-simulator (#1468)

--- test/unit/simulator-management-specs.js
+++ test/unit/simulator-management-specs.js
@@ -1,97 +1,15 @@
-import { createSim, getExistingSim, runSimulatorReset } from '../../lib/simulator-management.js';
-import sinon from 'sinon';
+import { runSimulatorReset } from '../../lib/simulator-management.js';
 import chai from 'chai';
-import Simctl from 'node-simctl';
 
 const should = chai.should();
 
-const caps = {platformName: 'iOS', deviceName: 'iPhone 6', platformVersion: '10.1', app: '/foo.app'};
-const iOSSimulatorModule = require('appium-ios-simulator');
-
 describe('simulator management', function () {
-  let getSimulatorStub = sinon.stub(iOSSimulatorModule, 'getSimulator');
-  afterEach(function () {
-    getSimulatorStub.reset();
-  });
-
-  describe('createSim', function () {
-    let createDeviceStub = sinon.stub(Simctl.prototype, 'createDevice');
-
-    afterEach(function () {
-      createDeviceStub.reset();
-    });
-
-    it('should call appiumTest prefix name', async function () {
-      createDeviceStub.returns('dummy-udid');
-      getSimulatorStub.returns('dummy-udid');
-
-      await createSim(caps);
-
-      createDeviceStub.calledOnce.should.be.true;
-      /appiumTest-[\w-]{36}-iPhone 6/.test(createDeviceStub.firstCall.args[0]).should.be.true;
-      createDeviceStub.firstCall.args[1].should.eql('iPhone 6');
-      createDeviceStub.firstCall.args[2].should.eql('10.1');
-      getSimulatorStub.calledOnce.should.be.true;
-      getSimulatorStub.firstCall.args[0].should.eql('dummy-udid');
-    });
-  });
-
-  describe('getExistingSim', function () {
-    let createDeviceStub = sinon.stub(Simctl.prototype, 'getDevices');
-
-    afterEach(function () {
-      createDeviceStub.reset();
-    });
-
-    it('should call default device name', async function () {
-      createDeviceStub.returns([{name: 'iPhone 6', udid: 'dummy-udid'}]);
-      getSimulatorStub.returns('dummy-udid');
-
-      await getExistingSim(caps);
-      createDeviceStub.calledOnce.should.be.true;
-      createDeviceStub.firstCall.args[0].should.eql('10.1');
-      getSimulatorStub.calledOnce.should.be.true;
-      getSimulatorStub.firstCall.args[0].should.eql('dummy-udid');
-    });
-
-    it('should call non-appiumTest prefix name if device has appiumTest prefix and no prefix device name', async function () {
-      createDeviceStub.returns([{name: 'appiumTest-iPhone 6', udid: 'appiumTest-dummy-udid'}, {name: 'iPhone 6', udid: 'dummy-udid'}]);
-      getSimulatorStub.returns('dummy-udid');
-
-      await getExistingSim(caps);
-      createDeviceStub.calledOnce.should.be.true;
-      createDeviceStub.firstCall.args[0].should.eql('10.1');
-      getSimulatorStub.calledOnce.should.be.true;
-      getSimulatorStub.firstCall.args[0].should.eql('dummy-udid');
-    });
-
-    it('should call appiumTest prefix device name', async function () {
-      createDeviceStub.returns([{name: 'appiumTest-iPhone 6', udid: 'dummy-udid'}]);
-      getSimulatorStub.returns('dummy-udid');
-
-      await getExistingSim(caps);
-      createDeviceStub.calledOnce.should.be.true;
-      createDeviceStub.firstCall.args[0].should.eql('10.1');
-      getSimulatorStub.calledOnce.should.be.true;
-      getSimulatorStub.firstCall.args[0].should.eql('dummy-udid');
-    });
-
-    it('should not exist sim', async function () {
-      createDeviceStub.returns();
-      getSimulatorStub.returns();
-
-      await getExistingSim(caps);
-      createDeviceStub.calledOnce.should.be.true;
-      createDeviceStub.firstCall.args[0].should.eql('10.1');
-      getSimulatorStub.notCalled.should.be.true;
-    });
-  });
   describe('runSimulatorReset', function () {
     let result;
     const stoppedDeviceDummy = {
       isRunning: () => false,
-      scrubCustomApp: (path, bundleId) => {
-        result = {path, bundleId};
+      scrubApp: (bundleId) => {
+        result = {bundleId};
       },
       clean: () => {
         result = 'cleaned';
@@ -103,14 +21,13 @@ describe('simulator management', function () {
       result = undefined;
     });
 
-    it('should call scrubCustomApp with fastReset', async function () {
+    it('should call scrubApp with fastReset', async function () {
       const opts = {
         udid: '301CD634-00A9-4042-B463-BD4E755167EA',
         bundleId: 'io.appium.example',
         noReset: false, fullReset: false
       };
       await runSimulatorReset(stoppedDeviceDummy, opts);
-      result.path.should.eql('');
       result.bundleId.should.eql('io.appium.example');
     });
     it('should return immediately with noReset', async function () {
@@ -131,7 +48,7 @@ describe('simulator management', function () {
       await runSimulatorReset(stoppedDeviceDummy, opts);
       result.should.eql('cleaned');
     });
-    it('should call scrubCustomApp with fastReset and app', async function () {
+    it('should not call scrubApp with fastReset and app', async function () {
       const opts = {
         udid: '301CD634-00A9-4042-B463-BD4E755167EA',
         bundleId: 'io.appium.example',
@@ -161,7 +78,7 @@ describe('simulator management', function () {
       await runSimulatorReset(stoppedDeviceDummy, opts);
       result.should.eql('cleaned');
     });
-    it('should not call scrubCustomApp with fastReset, but no bundleid and app', async function () {
+    it('should not call scrubApp with fastReset, but no bundleid and app', async function () {
       const opts = {
         udid: '301CD634-00A9-4042-B463-BD4E755167EA',
         noReset: false, fullReset: false
