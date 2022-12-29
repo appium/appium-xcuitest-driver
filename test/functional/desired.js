8216193c4de4786557959c2b0a39588c62f9eb69
mwakizaka <21286384+mwakizaka@users.noreply.github.com>
2022-12-29 17:56:23 +0900

    docs: address to the app repositories[ci skip]

--- test/functional/desired.js
+++ test/functional/desired.js
@@ -32,9 +32,9 @@ const LAUNCH_WITH_IDB = checkFeatureInEnv('LAUNCH_WITH_IDB');
 const SHOW_XCODE_LOG = checkFeatureInEnv('SHOW_XCODE_LOG');
 const APPS = {
   uiCatalogApp: path.resolve(__dirname, '..', 'assets',
-    `${IS_ABOVE_IOS13 ? 'UIKitCatalog' : 'UICatalog'}-iphonesimulator.app`),
-  iosTestApp: path.resolve(__dirname, '..', 'assets', 'TestApp-iphonesimulator.app'),
-  touchIdApp: path.resolve(__dirname, '..', 'assets', 'biometric.app')
+    `${IS_ABOVE_IOS13 ? 'UIKitCatalog' : 'UICatalog'}-iphonesimulator.app`), // https://github.com/appium/ios-uicatalog
+  iosTestApp: path.resolve(__dirname, '..', 'assets', 'TestApp-iphonesimulator.app'), // https://github.com/appium/ios-test-app
+  touchIdApp: path.resolve(__dirname, '..', 'assets', 'biometric.app') // https://github.com/mwakizaka/LocalAuthentication
 };
 
 
