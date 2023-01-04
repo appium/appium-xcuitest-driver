a9c34e6a24688a9350fbe0450cdf2f7fdd4fe0cb
Christopher Hiller <boneskull@boneskull.com>
2023-01-04 14:33:20 -0800

    feat: build docs with typedoc
    - Replaces Babel with TypeScript
    - Adds `@appium/typedoc-plugin-appium` and its requirements
    - Add `appium` as a dev dep ??

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -2,64 +2,62 @@
 /* eslint-disable promise/prefer-await-to-callbacks */
 /* eslint-disable promise/prefer-await-to-then */
 
-const {Mike} = require('@appium/docutils');
+const {buildReferenceDocs, deploy, updateNav} = require('@appium/docutils');
 const {logger} = require('@appium/support');
 const path = require('path');
 const semver = require('semver');
 const {version} = require('../package.json');
 
-
 const log = logger.getLogger('Docs');
 
 const DOCS_REMOTE = 'origin';
 const DOCS_BRANCH = 'docs-site';
-const DOCS_PREFIX = '';
+const DOCS_PREFIX = false;
 const REPO_DIR = path.resolve(__dirname, '..');
 const LATEST_ALIAS = 'latest';
 
-const branch = process.env.APPIUM_DOCS_BRANCH || DOCS_BRANCH;
-const prefix = process.env.APPIUM_DOCS_PREFIX || DOCS_PREFIX;
-const remote = process.env.APPIUM_DOCS_REMOTE || DOCS_REMOTE;
+const packageJson = require.resolve('../package.json');
+const typedocJson = require.resolve('../typedoc.json');
 
-const shouldPush = !!process.env.APPIUM_DOCS_PUBLISH;
+const branch = process.env.APPIUM_DOCS_BRANCH ?? DOCS_BRANCH;
+const prefix = process.env.APPIUM_DOCS_PREFIX ?? DOCS_PREFIX;
+const remote = process.env.APPIUM_DOCS_REMOTE ?? DOCS_REMOTE;
+
+const push = Boolean(process.env.APPIUM_DOCS_PUBLISH);
+const rebase = push;
 
 async function main() {
   log.info(`Building XCUI docs and committing to ${DOCS_BRANCH}`);
+  const {major, minor} = semver.parse(version);
+  const deployVersion = `${major}.${minor}`;
 
-  const semVersion = semver.parse(version);
-  const majMinVer = `${semVersion.major}.${semVersion.minor}`;
+  await buildReferenceDocs();
 
-  log.info(`Building docs for version ${majMinVer}`);
-  const configFile = path.join(REPO_DIR, `mkdocs.yml`);
-  const m = new Mike({
+  const mkdocsYml = path.join(REPO_DIR, 'mkdocs.yml');
+
+  log.info(`Updating nav in ${mkdocsYml}`);
+  await updateNav({mkdocsYml, typedocJson});
+
+  log.info(`Building docs for version ${deployVersion}`);
+  await deploy({
+    mkDocsYml: mkdocsYml,
+    push,
+    rebase,
     branch,
     prefix,
     remote,
-    configFile,
-  });
-
-  const docsAlreadyExisted = (await m.list()).length >= 1;
-
-  const deployOpts = {
-    version: majMinVer,
+    packageJson,
+    deployVersion,
+    message: `docs: auto-build docs for appium-xcuitest-driver@${deployVersion}`,
     alias: LATEST_ALIAS,
-    shouldRebase: shouldPush,
-    shouldPush,
-    commit: `docs: auto-build docs for appium-xcuitest-driver@${majMinVer}`,
-  };
-  await m.deploy(deployOpts);
-
-  if (!docsAlreadyExisted && shouldPush) {
-    log.info(`Docs did not already exist so setting the latest alias to default`);
-    await m.setDefault(LATEST_ALIAS);
-  }
+  });
   log.info(`Docs built`);
 }
 
 if (require.main === module) {
   main().catch((err) => {
     console.error(err); // eslint-disable-line no-console
-    process.exit(1);
+    process.exitCode = 1;
   });
 }
 
