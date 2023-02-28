2d1685b660e96e8834edc264216fd5efa955eeac
mwakizaka <21286384+mwakizaka@users.noreply.github.com>
2023-03-01 04:03:30 +0900

    test: Update safari-ssl-e2e tests for WebDriverIO compatibility (#1507)

--- test/functional/web/safari-ssl-e2e-specs.js
+++ test/functional/web/safari-ssl-e2e-specs.js
@@ -1,125 +1,135 @@
-// TODO: Update tests for WdIO compatibility
+import chai from 'chai';
+import os from 'os';
+import https from 'https';
+import chaiAsPromised from 'chai-as-promised';
+import B from 'bluebird';
+import { MOCHA_TIMEOUT, initSession, deleteSession, hasDefaultPrebuiltWDA } from '../helpers/session';
+import {
+  doesIncludeCookie, doesNotIncludeCookie, newCookie, oldCookie1
+} from './helpers';
+import { SAFARI_CAPS, amendCapabilities } from '../desired';
 
-// import chai from 'chai';
-// import chaiAsPromised from 'chai-as-promised';
-// import B from 'bluebird';
-// import { MOCHA_TIMEOUT, initSession, deleteSession } from '../helpers/session';
-// import {
-//   doesIncludeCookie, doesNotIncludeCookie, newCookie, oldCookie1
-// } from './helpers';
-// import { SAFARI_CAPS, amendCapabilities } from '../desired';
-// import https from 'https';
 
+const pem = B.promisifyAll(require('pem'));
 
-// const pem = B.promisifyAll(require('pem'));
+chai.should();
+chai.use(chaiAsPromised);
 
-// chai.should();
-// chai.use(chaiAsPromised);
+const HTTPS_PORT = 9762;
 
-// const HTTPS_PORT = 9762;
+const LOCAL_HTTPS_URL = `https://localhost:${HTTPS_PORT}/`;
 
-// const LOCAL_HTTPS_URL = `https://localhost:${HTTPS_PORT}/`;
+let caps;
+let pemCertificate;
 
-// const caps = amendCapabilities(SAFARI_CAPS, {
-//   'appium:safariInitialUrl': LOCAL_HTTPS_URL,
-//   'appium:noReset': true,
-// });
+describe('Safari SSL', function () {
+   this.timeout(MOCHA_TIMEOUT);
 
-// let pemCertificate;
+   let sslServer, driver;
+   before(async function () {
+     // Create a random pem certificate
+     const privateKey = await pem.createPrivateKeyAsync();
+     const keys = await pem.createCertificateAsync({
+       days: 1,
+       selfSigned: true,
+       serviceKey: privateKey.key,
+       altNames: ['localhost'],
+     });
+     pemCertificate = keys.certificate;
 
-// if (!process.env.REAL_DEVICE && !process.env.CLOUD) {
-//   describe.skip('Safari SSL', function () {
-//     this.timeout(MOCHA_TIMEOUT);
+     // Host an SSL server that uses that certificate
+     const serverOpts = {key: keys.serviceKey, cert: pemCertificate};
+     sslServer = https.createServer(serverOpts, (req, res) => {
+       res.end('Arbitrary text');
+     }).listen(HTTPS_PORT);
 
-//     let sslServer, driver;
-//     before(async function () {
-//       // Create a random pem certificate
-//       const privateKey = await pem.createPrivateKeyAsync();
-//       const keys = await pem.createCertificateAsync({
-//         days: 1,
-//         selfSigned: true,
-//         serviceKey: privateKey.key,
-//         altNames: ['localhost'],
-//       });
-//       pemCertificate = keys.certificate;
+     caps = amendCapabilities(SAFARI_CAPS, {
+       'appium:safariInitialUrl': LOCAL_HTTPS_URL,
+       'appium:noReset': true,
+       'appium:customSSLCert': pemCertificate + os.EOL,
+       'appium:usePrebuiltWDA': hasDefaultPrebuiltWDA(),
+     });
+   });
+   after(async function () {
+     await deleteSession();
+     if (sslServer) {
+       await sslServer.close();
+     }
+   });
 
-//       // Host an SSL server that uses that certificate
-//       const serverOpts = {key: keys.serviceKey, cert: pemCertificate};
-//       sslServer = https.createServer(serverOpts, (req, res) => {
-//         res.end('Arbitrary text');
-//       }).listen(HTTPS_PORT);
+   it('should open pages with untrusted certs if the cert was provided in desired capabilities', async function () {
+     try {
+       driver = await initSession(caps);
+       await driver.getPageSource().should.eventually.include('Arbitrary text');
+       await deleteSession();
+       await B.delay(1000);
 
-//       caps.customSSLCert = pemCertificate;
-//     });
-//     after(async function () {
-//       await deleteSession();
-//       if (sslServer) {
-//         await sslServer.close();
-//       }
-//     });
+       // Now do another session using the same cert to verify that it still works
+       driver = await initSession(caps);
+       await driver.url(LOCAL_HTTPS_URL);
+       await driver.getPageSource().should.eventually.include('Arbitrary text');
+     } finally {
+       await deleteSession();
+     }
+   });
 
-//     it('should open pages with untrusted certs if the cert was provided in desired capabilities', async function () {
-//       try {
-//         driver = await initSession(caps);
-//         await driver.source().should.eventually.include('Arbitrary text');
-//         await driver.quit();
-//         await B.delay(1000);
+   describe('cookies', function () {
+     const secureCookie = Object.assign({}, newCookie, {
+       secure: true,
+       name: 'securecookie',
+       value: 'this is a secure cookie',
+     });
 
-//         // Now do another session using the same cert to verify that it still works
-//         // (Don't do it on CLOUD. Restarting is too slow)
-//         if (!process.env.CLOUD) {
-//           await driver.init(caps);
-//           await driver.get(LOCAL_HTTPS_URL);
-//           await driver.source().should.eventually.include('Arbitrary text');
-//         }
-//       } finally {
-//         await deleteSession();
-//       }
-//     });
+     before(async function () {
+       driver = await initSession(caps);
+     });
 
-//     describe('cookies', function () {
-//       const secureCookie = Object.assign({}, newCookie, {
-//         secure: true,
-//         name: 'securecookie',
-//         value: 'this is a secure cookie',
-//       });
+     beforeEach(async function () {
+       await driver.url(LOCAL_HTTPS_URL);
+       await driver.setCookies([oldCookie1]);
+       await driver.deleteCookie(secureCookie.name);
+     });
 
-//       before(async function () {
-//         driver = await initSession(caps);
-//       });
+     it('should be able to set a secure cookie', async function () {
+       let cookies = await driver.getCookies();
+       doesNotIncludeCookie(cookies, secureCookie);
 
-//       beforeEach(async function () {
-//         await driver.get(LOCAL_HTTPS_URL);
-//         await driver.setCookie(oldCookie1);
-//         await driver.deleteCookie(secureCookie.name);
-//       });
+       await driver.setCookies([secureCookie]);
 
-//       it('should be able to set a secure cookie', async function () {
-//         let cookies = await driver.allCookies();
-//         doesNotIncludeCookie(cookies, secureCookie);
+       cookies = await driver.getCookies();
+       doesIncludeCookie(cookies, secureCookie);
+     });
 
-//         await driver.setCookie(secureCookie);
-//         cookies = await driver.allCookies();
+     it('should not delete an old cookie after setting a secure cookie', async function () {
+       let cookies = await driver.getCookies();
+       doesIncludeCookie(cookies, oldCookie1);
 
-//         doesIncludeCookie(cookies, secureCookie);
+       await driver.setCookies([secureCookie]);
 
-//         // should not clobber old cookie
-//         doesIncludeCookie(cookies, oldCookie1);
-//       });
-//       it('should be able to set a secure cookie', async function () {
-//         await driver.setCookie(secureCookie);
-//         let cookies = await driver.allCookies();
+       cookies = await driver.getCookies();
+       doesIncludeCookie(cookies, oldCookie1);
+     });
 
-//         doesIncludeCookie(cookies, secureCookie);
+     it('should be able to delete a secure cookie', async function () {
+       await driver.setCookies([secureCookie]);
+       let cookies = await driver.getCookies();
+       doesIncludeCookie(cookies, secureCookie);
 
-//         // should not clobber old cookie
-//         doesIncludeCookie(cookies, oldCookie1);
+       await driver.deleteCookie(secureCookie.name);
 
-//         await driver.deleteCookie(secureCookie.name);
+       cookies = await driver.getCookies();
+       doesNotIncludeCookie(cookies, secureCookie);
+     });
 
-//         cookies = await driver.allCookies();
-//         doesNotIncludeCookie(cookies, secureCookie);
-//       });
-//     });
-//   });
-// }
+     it('should not delete a cookie after deleting a secure cookie', async function () {
+       await driver.setCookies([secureCookie]);
+       let cookies = await driver.getCookies();
+       doesIncludeCookie(cookies, oldCookie1);
+
+       await driver.deleteCookie(secureCookie.name);
+
+       cookies = await driver.getCookies();
+       doesIncludeCookie(cookies, oldCookie1);
+     });
+   });
+ });
