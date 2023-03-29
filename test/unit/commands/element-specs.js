a8398da6bd74f32b1317cff7301855062da92a1b
Christopher Hiller <boneskull@boneskull.com>
2023-03-29 11:41:13 -0700

    chore(test): clean up unit test
    I cleaned up one of the unit tests and brought it up to the current building codes.  Thought this might be useful as a reference.
    
    Added `sinon-chai` and various needed types for type-checking tests, though only this one (`element-specs.js`) is actually checked.

--- test/unit/commands/element-specs.js
+++ test/unit/commands/element-specs.js
@@ -1,69 +1,84 @@
-import sinon from 'sinon';
-import XCUITestDriver from '../../../lib/driver';
+// @ts-check
+
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { withMocks, withSandbox } from '@appium/test-support';
-import _ from 'lodash';
-
+import {createSandbox} from 'sinon';
+import sinonChai from 'sinon-chai';
+import XCUITestDriver from '../../../lib/driver';
 
 chai.should();
-chai.use(chaiAsPromised);
+chai.use(chaiAsPromised).use(sinonChai);
 
 describe('element commands', function () {
-  let driver = new XCUITestDriver();
-  let proxyStub = sinon.stub(driver, 'proxyCommand');
+  /** @type {sinon.SinonSandbox} */
+  let sandbox;
 
-  afterEach(function () {
-    proxyStub.reset();
+  /** @type {XCUITestDriver} */
+  let driver;
+
+  /** @type {sinon.SinonStubbedMember<XCUITestDriver['proxyCommand']>} */
+  let proxyStub;
+
+  before(function () {
+    driver = new XCUITestDriver();
   });
 
-  describe('setValueImmediate', withMocks({driver}, (mocks) => {
-    afterEach(function () {
-      mocks.verify();
+  beforeEach(function () {
+    sandbox = createSandbox();
+    proxyStub = sandbox.stub(driver, 'proxyCommand');
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  describe('setValueImmediate', function () {
+    beforeEach(function () {
+      sandbox.stub(driver, 'setValue');
     });
+
     it('should call setValue', async function () {
-      mocks.driver
-        .expects('setValue')
-        .once().withExactArgs('hello', 2).returns();
       await driver.setValueImmediate('hello', 2);
+      driver.setValue.should.have.been.calledOnceWithExactly('hello', 2);
+      driver.setValue.should.have.returned(undefined);
     });
-  }));
+  });
 
   describe('getAttribute', function () {
     const elementId = 2;
     const attribute = 'enabled';
 
     afterEach(function () {
-      proxyStub.calledOnce.should.be.true;
+      proxyStub.should.have.been.calledOnce;
     });
 
     it('should properly parse boolean true attribute presented as integer', async function () {
-      proxyStub.returns(1);
+      proxyStub.resolves(1);
       (await driver.getAttribute(attribute, elementId)).should.eql('true');
     });
 
     it('should properly parse boolean false attribute presented as integer', async function () {
-      proxyStub.returns(0);
+      proxyStub.resolves(0);
       (await driver.getAttribute(attribute, elementId)).should.eql('false');
     });
 
     it('should properly parse integer attribute presented as string', async function () {
-      proxyStub.returns('0');
+      proxyStub.resolves('0');
       (await driver.getAttribute(attribute, elementId)).should.eql('0');
     });
 
     it('should properly parse boolean attribute presented as bool', async function () {
-      proxyStub.returns(false);
+      proxyStub.resolves(false);
       (await driver.getAttribute(attribute, elementId)).should.eql('false');
     });
 
     it('should properly parse null attribute', async function () {
-      proxyStub.returns(null);
-      _.isNull(await driver.getAttribute(attribute, elementId)).should.be.true;
+      proxyStub.resolves(null);
+      await chai.expect(driver.getAttribute(attribute, elementId)).to.eventually.be.null;
     });
 
     it('should properly parse string attribute', async function () {
-      proxyStub.returns('value');
+      proxyStub.resolves('value');
       (await driver.getAttribute(attribute, elementId)).should.eql('value');
     });
   });
@@ -73,64 +88,78 @@ describe('element commands', function () {
     const property = 'enabled';
 
     afterEach(function () {
-      proxyStub.calledOnce.should.be.true;
+      proxyStub.should.have.been.calledOnce;
     });
 
     it('should properly parse boolean true attribute presented as integer', async function () {
-      proxyStub.returns(1);
+      proxyStub.resolves(1);
       (await driver.getProperty(property, elementId)).should.eql('true');
     });
 
     it('should properly parse boolean false attribute presented as integer', async function () {
-      proxyStub.returns(0);
+      proxyStub.resolves(0);
       (await driver.getProperty(property, elementId)).should.eql('false');
     });
 
     it('should properly parse integer attribute presented as string', async function () {
-      proxyStub.returns('0');
+      proxyStub.resolves('0');
       (await driver.getProperty(property, elementId)).should.eql('0');
     });
 
     it('should properly parse boolean attribute presented as bool', async function () {
-      proxyStub.returns(false);
+      proxyStub.resolves(false);
       (await driver.getProperty(property, elementId)).should.eql('false');
     });
 
     it('should properly parse null attribute', async function () {
-      proxyStub.returns(null);
-      _.isNull(await driver.getProperty(property, elementId)).should.be.true;
+      proxyStub.resolves(null);
+      await chai.expect(driver.getProperty(property, elementId)).to.eventually.be.null;
     });
 
     it('should properly parse string attribute', async function () {
-      proxyStub.returns('value');
+      proxyStub.resolves('value');
       (await driver.getProperty(property, elementId)).should.eql('value');
     });
   });
 
-  describe('getAttribute - special contentSize', withSandbox({}, function (S) {
+  describe('getAttribute - special contentSize', function () {
     it('should call the internal method instead of WDA', async function () {
-      const getContentSizeStub = S.sandbox.stub(driver, 'getContentSize');
-      getContentSizeStub.returns('foo');
+      const getContentSizeStub = sandbox.stub(driver, 'getContentSize');
+      getContentSizeStub.resolves('foo');
       (await driver.getAttribute('contentSize', 2)).should.eql('foo');
-      proxyStub.called.should.be.false;
-      getContentSizeStub.calledOnce.should.be.true;
+      proxyStub.should.not.have.been.called;
+      getContentSizeStub.should.have.been.calledOnce;
     });
-  }));
+  });
 
-  describe('getContentSize', withSandbox({}, function (S) {
+  describe('getContentSize', function () {
     const el = {ELEMENT: '1234'};
-    let getAttrStub, getRectStub, findElStub, getSizeStub, getLocationStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['getAttribute']>} */
+    let getAttrStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['getElementRect']>} */
+    let getRectStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['findElOrEls']>} */
+    let findElStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['getSize']>} */
+    let getSizeStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['getLocationInView']>} */
+    let getLocationStub;
 
     beforeEach(function () {
-      getAttrStub = S.sandbox.stub(driver, 'getAttribute');
-      getRectStub = S.sandbox.stub(driver, 'getElementRect');
-      findElStub = S.sandbox.stub(driver, 'findElOrEls');
-      getSizeStub = S.sandbox.stub(driver, 'getSize');
-      getLocationStub = S.sandbox.stub(driver, 'getLocationInView');
+      getAttrStub = sandbox.stub(driver, 'getAttribute');
+      getRectStub = sandbox.stub(driver, 'getElementRect');
+      findElStub = sandbox.stub(driver, 'findElOrEls');
+      getSizeStub = sandbox.stub(driver, 'getSize');
+      getLocationStub = sandbox.stub(driver, 'getLocationInView');
     });
 
     describe('web context', function () {
-      const oldContext = driver.curContext;
+      /** @type {XCUITestDriver['curContext']} */
+      let oldContext;
+
+      beforeEach(function () {
+        oldContext = driver.curContext;
+      });
       beforeEach(function () {
         driver.curContext = 'WEBVIEW';
       });
@@ -143,73 +172,76 @@ describe('element commands', function () {
     });
 
     it('should throw if trying to get contentSize of something other than table or collection', async function () {
-      getAttrStub.returns('XCUIElementTypeStatusBar');
+      getAttrStub.resolves('XCUIElementTypeStatusBar');
       await driver.getContentSize(el).should.be.rejectedWith(/Can't get content size for type/);
     });
 
     it('should simply get the rect if just one child', async function () {
-      getAttrStub.returns('XCUIElementTypeTable');
-      findElStub.returns([{ELEMENT: 'foo'}]);
-      getRectStub.returns({x: 0, y: 0, height: 100, width: 200});
-      getSizeStub.returns({height: 100, width: 200});
-      getLocationStub.returns({x: 0, y: 0});
+      getAttrStub.resolves('XCUIElementTypeTable');
+      findElStub.resolves([{ELEMENT: 'foo'}]);
+      getRectStub.resolves({x: 0, y: 0, height: 100, width: 200});
+      getSizeStub.resolves({height: 100, width: 200});
+      getLocationStub.resolves({x: 0, y: 0});
       const contentSizeObj = JSON.parse(await driver.getContentSize(el));
       contentSizeObj.should.eql({
         width: 200,
         height: 100,
         top: 0,
         left: 0,
-        scrollableOffset: 100
+        scrollableOffset: 100,
       });
-      getRectStub.calledOnce.should.be.true;
+      getRectStub.should.have.been.calledOnce;
     });
 
     it('should get simple difference in element positions of a table', async function () {
-      const el1 = {ELEMENT: 1}, el2 = {ELEMENT: 2};
-      getAttrStub.returns('XCUIElementTypeTable');
-      findElStub.returns([el1, el2]);
-      getRectStub.withArgs(el1).returns({x: 0, y: 10, width: 50, height: 60});
-      getRectStub.withArgs(el2).returns({x: 10, y: 80, width: 60, height: 100});
-      getSizeStub.returns({height: 100, width: 200});
-      getLocationStub.returns({x: 0, y: 0});
+      const el1 = {ELEMENT: 1};
+      const el2 = {ELEMENT: 2};
+      getAttrStub.resolves('XCUIElementTypeTable');
+      findElStub.resolves([el1, el2]);
+      getRectStub.withArgs(el1).resolves({x: 0, y: 10, width: 50, height: 60});
+      getRectStub.withArgs(el2).resolves({x: 10, y: 80, width: 60, height: 100});
+      getSizeStub.resolves({height: 100, width: 200});
+      getLocationStub.resolves({x: 0, y: 0});
       const contentSizeObj = JSON.parse(await driver.getContentSize(el));
       contentSizeObj.should.eql({
         width: 200,
         height: 100,
         top: 0,
         left: 0,
-        scrollableOffset: 170
+        scrollableOffset: 170,
       });
-      getRectStub.calledTwice.should.be.true;
+      getRectStub.should.have.been.calledTwice;
     });
 
     it('should be sensitive to row items in the case of a collection view', async function () {
       // set up a collection view with 3 rows of 2 elements.
       // give the last row just one element
       const fixtures = [
-        {id: 1, x: 0, y: 0, height: 50}, {id: 2, x: 50, y: 0, height: 50},
-        {id: 3, x: 0, y: 60, height: 50}, {id: 4, x: 50, y: 60, height: 50},
-        {id: 5, x: 0, y: 120, height: 50}
+        {id: 1, x: 0, y: 0, height: 50, width: 50},
+        {id: 2, x: 50, y: 0, height: 50, width: 50},
+        {id: 3, x: 0, y: 60, height: 50, width: 50},
+        {id: 4, x: 50, y: 60, height: 50, width: 50},
+        {id: 5, x: 0, y: 120, height: 50, width: 50},
       ];
       const scrollableOffset = 170; // 3 rows plus space between two
-      getAttrStub.returns('XCUIElementTypeCollectionView');
-      findElStub.returns(fixtures.map((el) => ({ELEMENT: el.id})));
+      getAttrStub.resolves('XCUIElementTypeCollectionView');
+      findElStub.resolves(fixtures.map((el) => ({ELEMENT: el.id})));
       for (const item of fixtures) {
-        getRectStub.withArgs({ELEMENT: item.id}).returns(item);
+        getRectStub.withArgs({ELEMENT: item.id}).resolves(item);
       }
-      getSizeStub.returns({height: 100, width: 200});
-      getLocationStub.returns({x: 0, y: 0});
+      getSizeStub.resolves({height: 100, width: 200});
+      getLocationStub.resolves({x: 0, y: 0});
       const contentSizeObj = JSON.parse(await driver.getContentSize(el));
       contentSizeObj.should.eql({
         width: 200,
         height: 100,
         top: 0,
         left: 0,
-        scrollableOffset
+        scrollableOffset,
       });
-      getRectStub.callCount.should.equal(3);
+      getRectStub.should.have.been.calledThrice;
     });
-  }));
+  });
 
   describe('setValue', function () {
     const elementId = 2;
@@ -217,79 +249,72 @@ describe('element commands', function () {
     const expectedMethod = 'POST';
 
     describe('success', function () {
-      afterEach(function () {
-        proxyStub.calledOnce.should.be.true;
-        proxyStub.firstCall.args[0].should.eql(expectedEndpoint);
-        proxyStub.firstCall.args[1].should.eql(expectedMethod);
-      });
-
       it('should proxy string as array of characters', async function () {
         await driver.setValue('hello\uE006', elementId);
-        proxyStub.firstCall.args[2].should.eql({value: ['h', 'e', 'l', 'l', 'o', '\n']});
+        proxyStub.should.have.been.calledOnceWith(expectedEndpoint, expectedMethod, {
+          value: ['h', 'e', 'l', 'l', 'o', '\n'],
+        });
       });
       it('should proxy string with smileys as array of characters', async function () {
         await driver.setValue('hello😀😎', elementId);
-        proxyStub.firstCall.args[2].should.eql({value: ['h', 'e', 'l', 'l', 'o', '😀', '😎']});
+        proxyStub.should.have.been.calledOnceWith(expectedEndpoint, expectedMethod, {
+          value: ['h', 'e', 'l', 'l', 'o', '😀', '😎'],
+        });
       });
       it('should proxy number as array of characters', async function () {
         await driver.setValue(1234.56, elementId);
-        proxyStub.firstCall.args[2].should.eql({value: ['1', '2', '3', '4', '.', '5', '6']});
+        proxyStub.should.have.been.calledOnceWith(expectedEndpoint, expectedMethod, {
+          value: ['1', '2', '3', '4', '.', '5', '6'],
+        });
       });
       it('should proxy string array as array of characters', async function () {
         await driver.setValue(['hel', 'lo'], elementId);
-        proxyStub.firstCall.args[2].should.eql({value: ['h', 'e', 'l', 'l', 'o']});
+        proxyStub.should.have.been.calledOnceWith(expectedEndpoint, expectedMethod, {
+          value: ['h', 'e', 'l', 'l', 'o'],
+        });
       });
       it('should proxy integer array as array of characters', async function () {
         await driver.setValue([1234], elementId);
-        proxyStub.firstCall.args[2].should.eql({value: ['1', '2', '3', '4']});
+        proxyStub.should.have.been.calledOnceWith(expectedEndpoint, expectedMethod, {
+          value: ['1', '2', '3', '4'],
+        });
       });
     });
 
     describe('failure', function () {
       it('should throw invalid argument exception for null', async function () {
-        await driver.setValue(null, elementId)
-          .should.be.rejectedWith(/supported/);
+        await driver.setValue(null, elementId).should.be.rejectedWith(/supported/);
       });
       it('should throw invalid argument exception for object', async function () {
-        await driver.setValue({hi: 'there'}, elementId)
-          .should.be.rejectedWith(/supported/);
+        await driver.setValue({hi: 'there'}, elementId).should.be.rejectedWith(/supported/);
       });
     });
   });
 
   describe('getLocation for web elements', function () {
-    let driver = new XCUITestDriver();
-    const oldContext = driver.curContext;
-    const webEl = {ELEMENT: '5000'};
-    const fixtureXOffset = 100, fixtureYOffset = 200;
-    let executeStub;
-    let atomsElStub;
-    let atomStub;
-    let proxyStub;
+    /** @type {XCUITestDriver} */
+    let driver;
 
-    before(function () {
-      executeStub = sinon.stub(driver, 'execute').returns([fixtureXOffset, fixtureYOffset]);
-      atomsElStub = sinon.stub(driver, 'getAtomsElement').callsFake((el) => el);
-      atomStub = sinon.stub(driver, 'executeAtom').returns({x: 0, y: 0});
-      proxyStub = sinon.stub(driver, 'proxyCommand');
-    });
-    after(function () {
-      executeStub.reset();
-      atomsElStub.reset();
-      atomStub.reset();
-      proxyStub.reset();
-    });
+    const webEl = {ELEMENT: '5000'};
+    const fixtureXOffset = 100;
+    const fixtureYOffset = 200;
+
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['execute']>} */
+    let executeStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['executeAtom']>} */
+    let atomStub;
 
     beforeEach(function () {
+      driver = new XCUITestDriver();
       driver.curContext = 'fake web context';
+      executeStub = sandbox.stub(driver, 'execute').resolves([fixtureXOffset, fixtureYOffset]);
+      sandbox.stub(driver, 'getAtomsElement').resolvesArg(0);
+      atomStub = sandbox.stub(driver, 'executeAtom').resolves({x: 0, y: 0});
+      proxyStub = sandbox.stub(driver, 'proxyCommand');
     });
-    afterEach(function () {
-      driver.curContext = oldContext;
 
-      executeStub.resetHistory();
-      atomsElStub.resetHistory();
-      atomStub.resetHistory();
-      proxyStub.resetHistory();
+    afterEach(function () {
+      sandbox.restore();
     });
 
     it('should get location relative to scroll by default', async function () {
@@ -313,25 +338,33 @@ describe('element commands', function () {
   });
 
   describe('getElementRect', function () {
-    let driver = new XCUITestDriver();
-    const elem = {ELEMENT: '5000'};
-    const getNativeRectStub = sinon.stub(driver, 'getNativeRect').returns({x: 0, y: 50, width: 100, height: 200});
-    const getLocationStub = sinon.stub(driver, 'getLocation').returns({x: 0, y: 50});
-    const getSizeStub = sinon.stub(driver, 'getSize').returns({width: 100, height: 200});
+    /** @type {XCUITestDriver} */
+    let driver;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['getNativeRect']>} */
+    let getNativeRectStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['getLocationInView']>} */
+    let getLocationStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['getSize']>} */
+    let getSizeStub;
+    /** @type {sinon.SinonStubbedMember<XCUITestDriver['isWebContext']>} */
     let isWebContextStub;
+    const elem = {ELEMENT: '5000'};
+
+    beforeEach(function () {
+      driver = new XCUITestDriver();
+      getNativeRectStub = sandbox
+        .stub(driver, 'getNativeRect')
+        .resolves({x: 0, y: 50, width: 100, height: 200});
+      getLocationStub = sandbox.stub(driver, 'getLocation').resolves({x: 0, y: 50});
+      getSizeStub = sandbox.stub(driver, 'getSize').resolves({width: 100, height: 200});
+    });
 
     afterEach(function () {
-      getNativeRectStub.resetHistory();
-      getLocationStub.resetHistory();
-      getSizeStub.resetHistory();
-      proxyStub.resetHistory();
-      if (isWebContextStub) {
-        isWebContextStub.restore();
-      }
+      sandbox.restore();
     });
 
     it('should get element rect in native context', async function () {
-      isWebContextStub = sinon.stub(driver, 'isWebContext').returns(false);
+      isWebContextStub = sandbox.stub(driver, 'isWebContext').returns(false);
 
       const rect = await driver.getElementRect(elem);
 
@@ -346,7 +379,7 @@ describe('element commands', function () {
     });
 
     it('should get element rect in Web context', async function () {
-      isWebContextStub = sinon.stub(driver, 'isWebContext').returns(true);
+      isWebContextStub = sandbox.stub(driver, 'isWebContext').returns(true);
 
       const rect = await driver.getElementRect(elem);
 
