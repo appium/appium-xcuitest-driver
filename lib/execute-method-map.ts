4de83f9023b15fa9376ac8787aa7e1499378b836
Christopher Hiller <boneskull@boneskull.com>
2023-04-13 13:12:09 -0700

    fix(commands): fix startAudioRecording and stopAudioRecording execute methods These were missing from the execute method map, and also needed to be refactored

--- lib/execute-method-map.ts
+++ lib/execute-method-map.ts
@@ -438,4 +438,21 @@ export const executeMethodMap = {
   'mobile: shake': {
     command: 'mobileShake',
   },
+  'mobile: startAudioRecording': {
+    command: 'startAudioRecording',
+    params: {
+      required: ['audioInput'],
+      optional: [
+        'timeLimit',
+        'audioCodec',
+        'audioBitrate',
+        'audioChannels',
+        'audioRate',
+        'forceRestart',
+      ],
+    },
+  },
+  'mobile: stopAudioRecording': {
+    command: 'stopAudioRecording',
+  },
 } as const satisfies ExecuteMethodMap<XCUITestDriver>;
