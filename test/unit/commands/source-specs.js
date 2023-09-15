2c1b5df0bbe401a7554348237cb51aec8afbfb10
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-09-15 04:27:15 -0400

    chore: remove old implementation in get page source (#1988)
    * chore: remove old implementation
    
    * chore: remove unused deps
    
    * chore: simplify
    
    * fix: typehint
    
    * test: update tests
    
    * test: remove unused variables

--- test/unit/commands/source-specs.js
+++ test/unit/commands/source-specs.js
@@ -4,8 +4,6 @@ import XCUITestDriver from '../../../lib/driver';
 const xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>';
 const xmlBody = '<some-xml/>';
 const srcTree = `${xmlHeader}${xmlBody}`;
-const appiumHeadTag = '<AppiumAUT>';
-const appiumFootTag = '</AppiumAUT>';
 
 describe('source commands', function () {
   let driver = new XCUITestDriver();
@@ -19,14 +17,8 @@ describe('source commands', function () {
     it('should send translated GET request to WDA', async function () {
       await driver.getPageSource();
       proxyStub.calledOnce.should.be.true;
-      proxyStub.firstCall.args[0].should.eql('/source?scope=AppiumAUT');
+      proxyStub.firstCall.args[0].should.eql('/source?format=xml&scope=AppiumAUT');
       proxyStub.firstCall.args[1].should.eql('GET');
-    });
-    it('should insert received xml into AppiumAUT tags', async function () {
-      let src = await driver.getPageSource();
-      src.indexOf(xmlHeader).should.eql(0);
-      src.indexOf(appiumHeadTag).should.eql(xmlHeader.length);
-      src.indexOf(appiumFootTag).should.eql(srcTree.length + appiumHeadTag.length);
     });
   });
 });
