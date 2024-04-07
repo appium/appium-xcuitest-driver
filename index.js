1162837d4bfa40562b738edd33c7fd466bfabd07
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-04-07 14:19:13 -0700

    feat: export doctor in xcuitest driver

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import {XCUITestDriver} from './lib/driver';
 
 export {XCUITestDriver};
+
+export * as doctorRequired from './lib/doctor/required-checks';
+export * as doctorOptional from './lib/doctor/optional-checks';
+
 export default XCUITestDriver;
