bd0da29aee5b1b7772d4266545b0707087979cef
Mykola Mokhnach <mokhnach@gmail.com>
2024-12-13 21:21:46 +0100

    chore: Tune contextUpdate event generation

--- lib/commands/bidi/models.ts
+++ lib/commands/bidi/models.ts
@@ -1,17 +1,28 @@
 import type { LogEntryAddedEvent, ContextUpdatedEvent } from './types';
 import { NATIVE_WIN } from '../../utils';
-import { CONTEXT_UPDATED_EVENT, LOG_ENTRY_ADDED_EVENT } from './constants';
+import { CONTEXT_UPDATED_EVENT, CONTEXT_UPDATED_EVENT_OBSOLETE, LOG_ENTRY_ADDED_EVENT } from './constants';
 import type { LogEntry } from '../types';
 
-export function makeContextUpdatedEvent(contextName: string): ContextUpdatedEvent {
+function toContextUpdatedEvent(method: string, contextName: string): ContextUpdatedEvent {
   return {
-    method: CONTEXT_UPDATED_EVENT,
+    method,
     params: {
       name: contextName,
       type: contextName === NATIVE_WIN ? 'NATIVE' : 'WEB',
     },
   };
 }
+
+export const makeContextUpdatedEvent = (contextName: string) => toContextUpdatedEvent(
+  CONTEXT_UPDATED_EVENT, contextName
+);
+
+/**
+ * @deprecated Use {@link makeContextUpdatedEvent} instead
+ */
+export const makeObsoleteContextUpdatedEvent = (contextName: string) => toContextUpdatedEvent(
+  CONTEXT_UPDATED_EVENT_OBSOLETE, contextName
+);
 
 export function makeLogEntryAddedEvent(entry: LogEntry, context: string, type: string): LogEntryAddedEvent {
   return {
