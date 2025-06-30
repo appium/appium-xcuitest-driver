65cc86e8b6d6fe700cb79dd43ddbc86af78e47ca
saikrishna321 <saikrishna321@yahoo.com>
2025-06-30 12:45:22 +0530

    Fix linting
    Co-authored-by: SrinivasanTarget <srinivasan.sekar1990@gmail.com>

--- lib/commands/types.ts
+++ lib/commands/types.ts
@@ -3,7 +3,7 @@ import type B from 'bluebird';
 import type {EventEmitter} from 'node:events';
 import type {LiteralUnion, SetOptional, SetRequired} from 'type-fest';
 import type {Page} from '../types';
-import type {AuthorizationStatus, BatteryState, ThermalState} from './enum';
+import type {AuthorizationStatus, ThermalState} from './enum';
 
 export type Direction = 'up' | 'down' | 'left' | 'right';
 
