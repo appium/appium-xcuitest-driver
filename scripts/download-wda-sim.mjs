d5167e7030ab51e6cef48016481235a8e3393755
Mykola Mokhnach <mokhnach@gmail.com>
2025-07-09 19:20:17 +0200

    revert comments

--- scripts/download-wda-sim.mjs
+++ scripts/download-wda-sim.mjs
@@ -1,3 +1,5 @@
+// TODO: Please revert '@appium/support' to 'appium/support.js' and "@appium/support" dependency
+// in Appium 3 based version
 import {fs, logger, zip, net, node} from '@appium/support';
 import _ from 'lodash';
 import os from 'os';
