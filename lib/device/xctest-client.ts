66cad6b889c85680bb0e0b142aad4939288a6997
Mykola Mokhnach <mokhnach@gmail.com>
2026-04-13 20:28:13 +0200

    moar

--- lib/device/xctest-client.ts
+++ lib/device/xctest-client.ts
@@ -1,7 +1,6 @@
-import _ from 'lodash';
 import type {StringRecord} from '@appium/types';
 import type {XCUITestDriver} from '../driver';
-import type {XCTestResult, RunXCTestResult} from '../commands/types';
+import type {RunXCTestResult} from '../commands/types';
 import {
   runXCTestViaRemoteXPC,
   listXCTestBundlesViaRemoteXPC,
@@ -64,84 +63,6 @@ export class XCTestClient {
       throw new Error(XCTEST_REAL_DEVICE_MSG);
     }
   }
-}
-
-export function parseLegacyXCTestStdout(stdout: string): XCTestResult[] | string[] {
-  function parseKey(name: string): string {
-    const words = name.split(' ');
-    let out = '';
-    for (const word of words) {
-      out += word.substr(0, 1).toUpperCase() + word.substr(1);
-    }
-    return out.substr(0, 1).toLowerCase() + out.substr(1);
-  }
-
-  function parseValue(value: string): any {
-    value = value || '';
-    switch (value.toLowerCase()) {
-      case 'true':
-        return true;
-      case 'false':
-        return false;
-      case '':
-        return null;
-      default:
-        break;
-    }
-    if (!isNaN(Number(value))) {
-      if (!_.isString(value)) {
-        return 0;
-      } else if (value.indexOf('.') > 0) {
-        return parseFloat(value);
-      }
-      return parseInt(value, 10);
-    }
-    return value;
-  }
-
-  if (!stdout) {
-    return [];
-  }
-
-  const lines = stdout.trim().split('\n');
-  if (lines.length === 1 && !lines[0].includes('|')) {
-    return [lines[0]];
-  }
-
-  const results: XCTestResult[] = [];
-  for (const line of lines) {
-    const properties = line.split('|');
-    const output: any = {};
-    let entryIndex = 0;
-
-    for (const prop of properties) {
-      if (entryIndex === 0) {
-        output.testName = prop.trim();
-      } else if (prop.trim().startsWith('Location')) {
-        output.location = prop.substring(prop.indexOf('Location') + 8).trim();
-      } else {
-        const [key, value] = prop.split(':');
-        output[parseKey(key.trim())] = parseValue(value ? value.trim() : '');
-      }
-      entryIndex++;
-    }
-
-    if (!output.passed) {
-      output.passed = output.status === 'passed';
-      output.crashed = output.status === 'crashed';
-    } else if (!output.status) {
-      if (output.passed) {
-        output.status = 'passed';
-      } else if (output.crashed) {
-        output.status = 'crashed';
-      } else {
-        output.status = 'failed';
-      }
-    }
-
-    results.push(output);
-  }
-  return results;
 }
 
 interface XCTestClientDeps {
