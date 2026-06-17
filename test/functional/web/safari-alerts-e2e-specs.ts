fd07c202e9304ab35eb31d26b8639925c4e8192c
Mykola Mokhnach <mokhnach@gmail.com>
2026-06-17 11:16:23 +0200

    tune web tests

--- test/functional/web/safari-alerts-e2e-specs.ts
+++ test/functional/web/safari-alerts-e2e-specs.ts
@@ -7,6 +7,9 @@ import chaiAsPromised from 'chai-as-promised';
 
 chai.use(chaiAsPromised);
 
+const IS_CI = Boolean(process.env.CI);
+const CI_WEBVIEW_ATOM_WAIT_TIMEOUT_MS = 20000;
+
 describe('safari - alerts', function () {
   this.timeout(MOCHA_TIMEOUT);
 
@@ -18,6 +21,7 @@ describe('safari - alerts', function () {
     const caps = amendCapabilities(SAFARI_CAPS, {
       'appium:safariInitialUrl': guineaPigPage(baseUrl),
       'appium:safariAllowPopups': true,
+      ...(IS_CI ? {'appium:webviewAtomWaitTimeout': CI_WEBVIEW_ATOM_WAIT_TIMEOUT_MS} : {}),
     });
 
     if (process.env.CI && isIosVersionBelow('18.0')) {
@@ -25,6 +29,7 @@ describe('safari - alerts', function () {
     }
 
     driver = await initSession(caps);
+    await driver.setTimeout({implicit: 0});
   });
   after(async function () {
     await deleteSession();
@@ -39,28 +44,40 @@ describe('safari - alerts', function () {
     await retryInterval(5, 500, driver.dismissAlert.bind(driver));
   }
 
+  async function findWithRetry(selector: string) {
+    let element: any;
+    await retryInterval(IS_CI ? 8 : 5, 1000, async function () {
+      const els = await driver.$$(selector);
+      if (els.length === 0) {
+        throw new Error(`Element '${selector}' not found`);
+      }
+      element = els[0];
+    });
+    return element;
+  }
+
   it('should accept alert', async function () {
-    const alert = await driver.$('#alert1');
+    const alert = await findWithRetry('#alert1');
     await alert.click();
     await acceptAlert(driver);
     expect(await driver.getTitle()).to.include('I am a page title');
   });
 
   it('should dismiss alert', async function () {
-    const alert = await driver.$('#alert1');
+    const alert = await findWithRetry('#alert1');
     await alert.click();
     await dismissAlert(driver);
     expect(await driver.getTitle()).to.include('I am a page title');
   });
 
   it('should get text of alert', async function () {
-    const alert = await driver.$('#alert1');
+    const alert = await findWithRetry('#alert1');
     await alert.click();
     expect(await driver.getAlertText()).to.include('I am an alert');
     await dismissAlert(driver);
   });
   it('should not get text of alert that closed', async function () {
-    const alert = await driver.$('#alert1');
+    const alert = await findWithRetry('#alert1');
     await alert.click();
     await acceptAlert(driver);
     await expect(driver.getAlertText()).to.be.rejectedWith(
@@ -68,16 +85,16 @@ describe('safari - alerts', function () {
     );
   });
   it('should set text of prompt', async function () {
-    const alert = await driver.$('#prompt1');
+    const alert = await findWithRetry('#prompt1');
     await alert.click();
     await driver.sendAlertText('of course!');
     await acceptAlert(driver);
 
-    const el = await driver.$('#promptVal');
+    const el = await findWithRetry('#promptVal');
     expect(await el.getAttribute('value')).to.eql('of course!');
   });
   it('should fail to set text of alert', async function () {
-    const alert = await driver.$('#alert1');
+    const alert = await findWithRetry('#alert1');
     await alert.click();
     await expect(driver.sendAlertText('yes I do!')).to.be.rejectedWith(/no input fields/);
     await acceptAlert(driver);
